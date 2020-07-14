62c325ea270b30be1937a68e137e864bd0429bad
Ethan Hirsch <ethan.hirsch@intel.com>
2020-07-14 13:15:46 -0700

    added stb includes

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../stb/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../stb/stb_image_write.h"
+
 int main(){
     std::cout << "Hello World!" << std::endl;
     return 0;
