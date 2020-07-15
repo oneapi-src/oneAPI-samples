139c49ecb9efb9764ba11ccfce09d89428906fcf
JoeOster <52936608+JoeOster@users.noreply.github.com>
2020-07-15 13:51:19 -0700

    Update README.md (#24)
    * Update README.md
    
    * Rename README.md to third-party-programs.md
    
    * Create README.md
    
    * Create readme.md
    
    * Add files via upload

---
+++ common/dpc_common.hpp
@@ -0,0 +1,52 @@
+//==============================================================
+// Copyright © 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+// =============================================================
+
+#ifndef _DP_HPP
+#define _DP_HPP
+
+#pragma once
+
+#include <stdlib.h>
+#include <exception>
+
+#include <CL/sycl.hpp>
+
+namespace dpc_common {
+// this exception handler with catch async exceptions
+static auto exception_handler = [](cl::sycl::exception_list eList) {
+  for (std::exception_ptr const &e : eList) {
+    try {
+      std::rethrow_exception(e);
+    } catch (std::exception const &e) {
+#if _DEBUG
+      std::cout << "Failure" << std::endl;
+#endif
+      std::terminate();
+    }
+  }
+};
+
+// The TimeInterval is a simple RAII class.
+// Construct the timer at the point you want to start timing.
+// Use the Elapsed() method to return time since construction.
+
+class TimeInterval {
+ public:
+  TimeInterval() : start_(std::chrono::steady_clock::now()) {}
+
+  double Elapsed() {
+    auto now = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<Duration>(now - start_).count();
+  }
+
+ private:
+  using Duration = std::chrono::duration<double>;
+  std::chrono::steady_clock::time_point start_;
+};
+
+};  // namespace dpc_common
+
+#endif
