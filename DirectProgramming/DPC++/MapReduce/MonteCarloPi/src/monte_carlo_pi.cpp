b83075256ac5ef4af73e8649e81124db54f60910
Ethan Hirsch <ethan.hirsch@intel.com>
2020-07-15 15:11:50 -0700

    now draws sampled coordinates to the image
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -9,25 +9,32 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../stb/stb_image_write.h"
 
-#define SIZE_N 5 // number of simulated samples
+#define SIZE_N 1000 // number of simulated samples
 #define IMG_DIMENSIONS 512 //must be even
+#define RADIUS IMG_DIMENSIONS / 2
 #define CIRCLE_OUTLINE 0.025
 
 #define PI 3.14159265
 
+// Returns the pixel index corresponding to a set of simulation coordinates
+int GetIndex(float x, float y){
+    int img_x = x * RADIUS + RADIUS;
+    int img_y = y * RADIUS + RADIUS;
+    return img_y * IMG_DIMENSIONS + img_x;
+}
+
 // Returns a random float between -1 and 1
 float GetRandCoordinate(){
-    return (float)rand() / (RAND_MAX / 2) - 1.0;
+    return (float)rand() / (RAND_MAX / 2.0) - 1.0;
 }
 
 // Creates an array representing the image data and inscribes a circle
 rgb* CreatePlot(){
-    constexpr int radius = IMG_DIMENSIONS / 2;
     rgb* plot = (rgb*) calloc(IMG_DIMENSIONS * IMG_DIMENSIONS, sizeof(rgb));
     for (int i = 0; i < IMG_DIMENSIONS * IMG_DIMENSIONS; ++i){
         // calculate unit coordinates relative to the center of the image
-        float x = (float)(i % IMG_DIMENSIONS - radius) / radius;
-        float y = (float)(i / IMG_DIMENSIONS - radius) / radius;
+        float x = (float)(i % IMG_DIMENSIONS - RADIUS) / RADIUS;
+        float y = (float)(i / IMG_DIMENSIONS - RADIUS) / RADIUS;
         // draw the circumference of the circle
         if ((x * x + y * y) > 1 - CIRCLE_OUTLINE && (x * x + y * y) < 1) {
             plot[i].red = 255;
@@ -45,12 +52,23 @@ int main(){
     // Create image plot, and draw the circle
     rgb* image_plot = CreatePlot();
 
-
+    // Perform Monte Carlo simulation to estimate pi
     int count = 0;
     for (int i = 0; i < SIZE_N; ++i){
         float rand_x = GetRandCoordinate();
         float rand_y = GetRandCoordinate();
-        std::cout << "X: " << rand_x << " Y: " << rand_y << std::endl;
+        float hypotenuse_sqr = (rand_x * rand_x + rand_y * rand_y);
+        if (hypotenuse_sqr <= 1.0){
+            ++count;
+            image_plot[GetIndex(rand_x, rand_y)].red = 0;
+            image_plot[GetIndex(rand_x, rand_y)].green = 255;
+            image_plot[GetIndex(rand_x, rand_y)].blue = 0;
+        }
+        else{
+            image_plot[GetIndex(rand_x, rand_y)].red = 255;
+            image_plot[GetIndex(rand_x, rand_y)].green = 0;
+            image_plot[GetIndex(rand_x, rand_y)].blue = 0;
+        }
     }
 
 
