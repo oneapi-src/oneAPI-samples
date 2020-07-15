bcd63ec6332d8a9ae7a4ffa2c3e5a9d74c1d505c
Ethan Hirsch <ethan.hirsch@intel.com>
2020-07-15 14:58:02 -0700

    adding monte carlo simulation
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 #include "rgb.hpp"
 #define STB_IMAGE_IMPLEMENTATION
@@ -7,16 +9,22 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../stb/stb_image_write.h"
 
+#define SIZE_N 5 // number of simulated samples
 #define IMG_DIMENSIONS 512 //must be even
 #define CIRCLE_OUTLINE 0.025
 
 #define PI 3.14159265
 
+// Returns a random float between -1 and 1
+float GetRandCoordinate(){
+    return (float)rand() / (RAND_MAX / 2) - 1.0;
+}
+
 // Creates an array representing the image data and inscribes a circle
 rgb* CreatePlot(){
     constexpr int radius = IMG_DIMENSIONS / 2;
     rgb* plot = (rgb*) calloc(IMG_DIMENSIONS * IMG_DIMENSIONS, sizeof(rgb));
-    for (int i = 0; i < IMG_DIMENSIONS * IMG_DIMENSIONS; i++){
+    for (int i = 0; i < IMG_DIMENSIONS * IMG_DIMENSIONS; ++i){
         // calculate unit coordinates relative to the center of the image
         float x = (float)(i % IMG_DIMENSIONS - radius) / radius;
         float y = (float)(i / IMG_DIMENSIONS - radius) / radius;
@@ -31,13 +39,19 @@ rgb* CreatePlot(){
 }
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    // Initialize random seed
+    srand(time(NULL));
 
     // Create image plot, and draw the circle
-    constexpr int radius = IMG_DIMENSIONS / 2;
     rgb* image_plot = CreatePlot();
 
 
+    int count = 0;
+    for (int i = 0; i < SIZE_N; ++i){
+        float rand_x = GetRandCoordinate();
+        float rand_y = GetRandCoordinate();
+        std::cout << "X: " << rand_x << " Y: " << rand_y << std::endl;
+    }
 
 
 
