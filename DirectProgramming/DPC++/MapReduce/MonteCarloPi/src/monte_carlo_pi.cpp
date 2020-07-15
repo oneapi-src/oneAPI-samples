12f87912d86df40796b95665a692aa033765c346
Ethan Hirsch <ethan.hirsch@intel.com>
2020-07-15 11:41:14 -0700

    precision is now a constant
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -16,8 +16,7 @@ int main(){
 
     // Create image plot, and draw the circle
     constexpr int radius = IMG_DIMENSIONS / 2;
-    //constexpr float precision = 5.0 / IMG_DIMENSIONS;
-    constexpr float precision = 0.25;
+    constexpr float precision = 0.025;
     rgb* image_plot = (rgb*) calloc(IMG_DIMENSIONS * IMG_DIMENSIONS, sizeof(rgb));
     for (int i = 0; i < IMG_DIMENSIONS * IMG_DIMENSIONS; i++){
         // calculate unit coordinates relative to the center of the image
