3c820aef6526f10b7afffc83a6e5c6b01bdaf4ff
Ethan Hirsch <ethan.hirsch@intel.com>
2020-07-17 13:29:46 -0700

    swapped #defined constants for constexpr
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -12,17 +12,16 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../stb/stb_image_write.h"
 
-#define SIZE_N 10000 // number of simulated samples
-#define IMG_DIMENSIONS 1024 //must be even
-#define CIRCLE_OUTLINE 0.025
-
-constexpr int radius = IMG_DIMENSIONS / 2;
+constexpr int size_n = 10000;
+constexpr int img_dimensions = 1024;
+constexpr int radius = img_dimensions / 2;
+constexpr double circle_outline = 0.025;
 
 // Returns the pixel index corresponding to a set of simulation coordinates
 int GetIndex(double x, double y){
     int img_x = x * radius + radius;
     int img_y = y * radius + radius;
-    return img_y * IMG_DIMENSIONS + img_x;
+    return img_y * img_dimensions + img_x;
 }
 
 // Returns a random double between -1 and 1
@@ -31,13 +30,13 @@ double GetRandCoordinate(){
 }
 
 // Creates an array representing the image data and inscribes a circle
-rgb* CreatePlot(rgb * plot){
-    for (int i = 0; i < IMG_DIMENSIONS * IMG_DIMENSIONS; ++i){
+rgb* DrawPlot(rgb * plot){
+    for (int i = 0; i < img_dimensions * img_dimensions; ++i){
         // calculate unit coordinates relative to the center of the image
-        double x = (double)(i % IMG_DIMENSIONS - radius) / radius;
-        double y = (double)(i / IMG_DIMENSIONS - radius) / radius;
+        double x = (double)(i % img_dimensions - radius) / radius;
+        double y = (double)(i / img_dimensions - radius) / radius;
         // draw the circumference of the circle
-        if ((x * x + y * y) > 1 - CIRCLE_OUTLINE && (x * x + y * y) < 1) {
+        if ((x * x + y * y) > 1 - circle_outline && (x * x + y * y) < 1) {
             plot[i].red = 255;
             plot[i].green = 255;
             plot[i].blue = 255;
@@ -50,13 +49,15 @@ int main(){
     // Initialize random seed
     srand(time(NULL));
 
-    // Create image plot, and draw the circle
-    rgb* image_plot = (rgb*) calloc(IMG_DIMENSIONS * IMG_DIMENSIONS, sizeof(rgb));
-    CreatePlot(image_plot);
+    // Create image plot
+    rgb* image_plot = (rgb*) calloc(img_dimensions * img_dimensions, sizeof(rgb));
+
+    // Draw the inscribed circle for the plot
+    DrawPlot(image_plot);
 
     // Perform Monte Carlo simulation to estimate pi
     int count = 0;
-    for (int i = 0; i < SIZE_N; ++i){
+    for (int i = 0; i < size_n; ++i){
         double rand_x = GetRandCoordinate();
         double rand_y = GetRandCoordinate();
         double hypotenuse_sqr = (rand_x * rand_x + rand_y * rand_y);
@@ -73,11 +74,11 @@ int main(){
         }
     }
 
-    std::cout << "The estimated value of pi is: " << 4.0 * (double) count / SIZE_N << std::endl;
+    std::cout << "The estimated value of pi is: " << 4.0 * (double) count / size_n << std::endl;
 
 
     // Write image to file
-    stbi_write_bmp("MonteCarloPi.bmp", IMG_DIMENSIONS, IMG_DIMENSIONS, 3, image_plot);
+    stbi_write_bmp("MonteCarloPi.bmp", img_dimensions, img_dimensions, 3, image_plot);
 
     return 0;
 }
