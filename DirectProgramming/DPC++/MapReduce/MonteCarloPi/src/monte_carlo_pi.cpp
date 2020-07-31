c9e7a1b04342370c58dcc25cda658a06bf249097
Ethan Hirsch <ethan.hirsch@intel.com>
2020-07-31 11:34:27 -0700

    added sycl constructs
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -49,27 +49,45 @@ rgb* DrawPlot(rgb * image_plot){
 
 // performs the Monte Carlo simulation procedure for calculating pi, with size_n number of samples.
 void MonteCarloPi(rgb * image_plot){
-    int count = 0;
-    for (int i = 0; i < size_n; ++i){
-        double rand_x = GetRandCoordinate();
-        double rand_y = GetRandCoordinate();
-        double hypotenuse_sqr = (rand_x * rand_x + rand_y * rand_y);
-        if (hypotenuse_sqr <= 1.0){
-            ++count;
-            image_plot[GetIndex(rand_x, rand_y)].red = 0;
-            image_plot[GetIndex(rand_x, rand_y)].green = 255;
-            image_plot[GetIndex(rand_x, rand_y)].blue = 0;
-        }
-        else{
-            image_plot[GetIndex(rand_x, rand_y)].red = 255;
-            image_plot[GetIndex(rand_x, rand_y)].green = 0;
-            image_plot[GetIndex(rand_x, rand_y)].blue = 0;
-        }
-    }
+    // Set up sycl queue
+    queue q(default_selector{}, dpc_common::exception_handler);
+    std::cout << "Running on "
+            << q.get_device().get_info<sycl::info::device::name>() << std::endl;
+    
+    try{
+        // Array for reduction stage
+        float reduction_arr[size_n];
 
-    // Print calculated value of pi
-    double pi = 4.0 * (double) count / size_n;
-    std::cout << "The estimated value of pi is: " << pi << std::endl;
+        // Set up buffers
+        buffer<rgb, 1> imgplot_buf((rgb*)image_plot, range<1>(img_dimensions * img_dimensions));
+        buffer<float, 1> reduce_buf((float*)reduction_arr, range<1>(size_n));
+
+        //Monte Carlo sim procedure
+        int count = 0;
+        for (int i = 0; i < size_n; ++i){
+            double rand_x = GetRandCoordinate();
+            double rand_y = GetRandCoordinate();
+            double hypotenuse_sqr = (rand_x * rand_x + rand_y * rand_y);
+            if (hypotenuse_sqr <= 1.0){
+                ++count;
+                image_plot[GetIndex(rand_x, rand_y)].red = 0;
+                image_plot[GetIndex(rand_x, rand_y)].green = 255;
+                image_plot[GetIndex(rand_x, rand_y)].blue = 0;
+            }
+            else{
+                image_plot[GetIndex(rand_x, rand_y)].red = 255;
+                image_plot[GetIndex(rand_x, rand_y)].green = 0;
+                image_plot[GetIndex(rand_x, rand_y)].blue = 0;
+            }
+        }
+
+        // Print calculated value of pi
+        double pi = 4.0 * (double) count / size_n;
+        std::cout << "The estimated value of pi is: " << pi << std::endl;
+    } catch (sycl::exception e) {
+        cout << "SYCL exception caught: " << e.what() << "\n";
+        exit(1);
+    }
 }
 
 int main(){
