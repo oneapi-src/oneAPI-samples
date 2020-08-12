1eeb3ac4591361b938a22ff052cbce86f9331eb4
Ethan Hirsch <ethan.hirsch@intel.com>
2020-08-12 14:53:25 -0700

    more
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -141,7 +141,7 @@ void MonteCarloPi(rgb * image_plot){
 
     std::cout << "\n---------------------\n" << std::endl;
     for (int i = 0; i < size_n; i++){
-        cout << "HYPO: " << coords[i].x * coords[i].x + coords[i].y * coords[i].y << " VAL: " << reduction_arr[i] << std::endl;
+        std::cout << "HYPO: " << coords[i].x * coords[i].x + coords[i].y * coords[i].y << " VAL: " << reduction_arr[i] << std::endl;
     }
     std::cout << "\n---------------------\n" << std::endl;
 
