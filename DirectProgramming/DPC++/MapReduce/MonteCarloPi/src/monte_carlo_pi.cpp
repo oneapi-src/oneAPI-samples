cce5a6b063a63ab76df7575c2b3df7c72c1b2b41
Ethan Hirsch <ethan.hirsch@intel.com>
2020-08-12 10:37:13 -0700

    includes proper stb ref now
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -9,9 +9,9 @@
 #include "monte_carlo_pi.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "../stb/stb_image.h"
+#include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../stb/stb_image_write.h"
+#include "stb/stb_image_write.h"
 
 using namespace sycl;
 
