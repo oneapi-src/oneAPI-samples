c1636e69ca639964a16d88dc7e2a1988bbbe20f0
Ethan Hirsch <ethan.hirsch@intel.com>
2020-08-12 17:08:50 -0700

    big nombor
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -18,7 +18,7 @@ using namespace sycl;
 // size of parallel work groups
 constexpr int size_wg = 32;
 // Number of sample points
-constexpr int size_n = size_wg * 32768; // Must be a multiple of size_wg
+constexpr int size_n = size_wg * 20000; // Must be a multiple of size_wg
 // Output image dimensions
 constexpr int img_dimensions = 1024;
 
