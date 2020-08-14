74e5ec0d90cca66e0ba5228d90f6c5a63fb8cb16
Loc Nguyen <loc.q.nguyen@intel.com>
2020-08-14 13:34:27 -0700

    Add comment about the location of dpc_common.hpp.

--- DirectProgramming/DPC++/GraphTraversal/bitonic-sort/src/bitonic-sort.cpp
+++ DirectProgramming/DPC++/GraphTraversal/bitonic-sort/src/bitonic-sort.cpp
@@ -36,6 +36,9 @@
 //
 #include <math.h>
 #include <iostream>
+
+// dpc_common.hpp can be found in the dev-utilities include folder.
+// e.g., $ONEAPI_ROOT/dev-utilities/<version>/include/dpc_common.hpp
 #include "dpc_common.hpp"
 
 using namespace sycl;
