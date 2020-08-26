b48dc81daf1af462f2a5a7c5e39477de24341694
Mihai Tudor Panu <mihai.tudor.panu@intel.com>
2020-08-26 12:56:57 -0700

    hello-iot-world: update C++ compiler name throughout
    Signed-off-by: Mihai Tudor Panu <mihai.tudor.panu@intel.com>

--- Tools/IoTConnectionTools/hello-iot-world/cpp/hello-iot-world.cpp
+++ Tools/IoTConnectionTools/hello-iot-world/cpp/hello-iot-world.cpp
@@ -12,10 +12,10 @@ using namespace std;
 int main() {
   cout << "Hello, Internet of Things World!" << endl;
 #ifdef __INTEL_COMPILER
-  cout << "The Intel(R) C++ Compiler was used for compiling this sample."
+  cout << "The Intel(R) C++ Compiler Classic was used for compiling this sample."
        << endl;
 #else
-  cout << "The Intel(R) C++ Compiler was not used for compiling this sample."
+  cout << "The Intel(R) C++ Compiler Classic was not used for compiling this sample."
        << endl;
 #endif
   return 0;
