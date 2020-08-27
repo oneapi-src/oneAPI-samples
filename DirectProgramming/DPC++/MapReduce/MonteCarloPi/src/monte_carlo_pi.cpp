758970cba6c1fddd02929482fba1f1e50a8e20ee
Ethan Hirsch <ethan.hirsch@intel.com>
2020-08-27 09:11:14 -0700

    namespace
    Signed-off-by: Ethan Hirsch <ethan.hirsch@intel.com>

--- DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
+++ DirectProgramming/DPC++/MapReduce/MonteCarloPi/src/monte_carlo_pi.cpp
@@ -148,7 +148,7 @@ void MonteCarloPi(rgb * image_plot){
             // Reduction kernel
             auto a_acc = test_buf.get_access<access::mode::read>(h);
             auto total_acc = total_buf.get_access<access::mode::read_write>(h);
-            h.parallel_for(nd_range<1>(size_n, 10), reduction(total_acc, 0, plus<int>()), [=](auto it, auto& total_acc)
+            h.parallel_for(nd_range<1>(size_n, 10), reduction(total_acc, 0, std::plus<int>()), [=](auto it, auto& total_acc)
             {
                 total_acc[0] += a_acc[it];
             });
