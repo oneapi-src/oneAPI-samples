161a0919022a3e3004582b1b7348bd47c2a51267
clevels <59889830+clevels@users.noreply.github.com>
2020-09-09 16:56:45 -0500

    TBB Samples Migration Changes (Samples have already been merged and passed testing) (#133)
    * TBB Samples Migration
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * Addressing PR Change Requests
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * Fill in "Purpose" Section of both README files.
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * Remove binary and build files
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * include dpc_common header, remove exception handler, fix json files. (all changes apply to both samples)
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * include dpc_common headers, remove exception handlers (both samples)
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * Fix README files, include header files for windows
    
    * Remove namespace, end files, use "std::iota", fix README
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * fix README
    
    Signed-off-by: root <root@dtc-nuc-03l.jf.intel.com>
    
    * Fix "matrix_multiply" samples failures on Windows.
    
    Co-authored-by: root <root@dtc-nuc-03l.jf.intel.com>

--- Libraries/oneTBB/tbb-task-sycl/src/tbb-task-sycl.cpp
+++ Libraries/oneTBB/tbb-task-sycl/src/tbb-task-sycl.cpp
@@ -17,8 +17,6 @@
 // e.g., $ONEAPI_ROOT/dev-utilities//include/dpc_common.hpp
 #include "dpc_common.hpp"
 
-using namespace cl::sycl;
-
 constexpr cl::sycl::access::mode sycl_read = cl::sycl::access::mode::read;
 constexpr cl::sycl::access::mode sycl_write = cl::sycl::access::mode::write;
 constexpr cl::sycl::access::mode sycl_read_write = cl::sycl::access::mode::read_write;
@@ -46,18 +44,18 @@ class ExecuteOnGpu {
     {  // starting SYCL code
 
       const float coeff = alpha;  // coeff is a local varaible
-      range<1> n_items{array_size};
-      buffer<cl_float, 1> a_buffer(a_array.data(), n_items);
-      buffer<cl_float, 1> b_buffer(b_array.data(), n_items);
-      buffer<cl_float, 1> c_buffer(c_array.data(), n_items);
+      cl::sycl::range<1> n_items{array_size};
+      cl::sycl::buffer<cl_float, 1> a_buffer(a_array.data(), n_items);
+      cl::sycl::buffer<cl_float, 1> b_buffer(b_array.data(), n_items);
+      cl::sycl::buffer<cl_float, 1> c_buffer(c_array.data(), n_items);
 
-      queue q(default_selector{}, dpc_common::exception_handler);
-      q.submit([&](handler& h) {
+      cl::sycl::queue q(cl::sycl::default_selector{}, dpc_common::exception_handler);
+      q.submit([&](cl::sycl::handler& h) {
             auto a_accessor = a_buffer.get_access<sycl_read>(h);
             auto b_accessor = b_buffer.get_access<sycl_read>(h);
             auto c_accessor = c_buffer.get_access<sycl_write>(h);
 
-            h.parallel_for(n_items, [=](id<1> index) {
+            h.parallel_for(n_items, [=](cl::sycl::id<1> index) {
               c_accessor[index] = a_accessor[index] + b_accessor[index] * coeff;
             });  // end of the kernel -- parallel for
           })
@@ -93,10 +91,8 @@ void PrintArr(const char* text, const std::array<float, array_size>& array) {
 
 int main() {
   // init input arrays
-  for (int i = 0; i < array_size; i++) {
-    a_array[i] = i;
-    b_array[i] = i;
-  }
+  std::iota(a_array.begin(), a_array.end(), 0);
+  std::iota(b_array.begin(), b_array.end(), 0);
 
   // start tbb task group
   tbb::task_group tg;
