b1f66bd01c704b9a6e92c20819afa573c01fe5e9
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2020-09-16 16:59:50 +0300

    Change the sample

--- Libraries/oneTBB/tbb-resumable-tasks-sycl/src/tbb-resumable-tasks-sycl.cpp
+++ Libraries/oneTBB/tbb-resumable-tasks-sycl/src/tbb-resumable-tasks-sycl.cpp
@@ -1,17 +1,19 @@
 //==============================================================
-// Copyright © 2020 Intel Corporation
+// Copyright © 2019 Intel Corporation
 //
 // SPDX-License-Identifier: MIT
 // =============================================================
 
-#include <cmath> // for std::ceil
+#include <cmath>
 #include <array>
 #include <thread>
 #include <iostream>
+#include <atomic>
 #include <sstream>
 
 #include <CL/sycl.hpp>
 
+#include <tbb/blocked_range.h>
 #include <tbb/global_control.h>
 #include <tbb/task.h>
 #include <tbb/task_group.h>
@@ -34,35 +36,56 @@ void print_array( const char* text, const std::array<float, array_size>& array )
     std::cout << std::endl;
 }
 
-void run_sycl_activity( float offload_ratio ) {
-    std::size_t array_size_sycl = std::ceil(array_size * offload_ratio);
-    std::stringstream sstream;
-    sstream << "start index for GPU = 0; end index for GPU = "
-            << array_size_sycl << std::endl;
-    std::cout << sstream.str();
-    const float coeff = alpha; // coeff is a local variable
+class AsyncActivity {
+    float offload_ratio;
+    std::atomic<bool> submit_flag;
+    tbb::task::suspend_point suspend_point;
+    std::thread service_thread;
 
-    { // starting SYCL code
-        cl::sycl::range<1> n_items{array_size_sycl};
-        cl::sycl::buffer<cl_float, 1> a_buffer(a_array.data(), n_items);
-        cl::sycl::buffer<cl_float, 1> b_buffer(b_array.data(), n_items);
-        cl::sycl::buffer<cl_float, 1> c_buffer(c_array.data(), n_items);
+public:
+    AsyncActivity() : offload_ratio(0), submit_flag(false),
+        service_thread([this] {
+            while(!submit_flag) {
+                std::this_thread::yield();
+            }
+            std::size_t array_size_sycl = std::ceil(array_size * offload_ratio);
+            std::stringstream sstream;
+            sstream << "start index for GPU = 0; end index for GPU = "
+                    << array_size_sycl << std::endl;
+            std::cout << sstream.str();
+            const float coeff = alpha; // coeff is a local variable
 
-        cl::sycl::queue q;
-        tbb::task::suspend([&](tbb::task::suspend_point sus_point) {
-            q.submit([&](cl::sycl::handler& h) {
-                auto a_accessor = a_buffer.get_access<sycl_read>(h);
-                auto b_accessor = b_buffer.get_access<sycl_read>(h);
-                auto c_accessor = c_buffer.get_access<sycl_write>(h);
+            { // starting SYCL code
+                cl::sycl::range<1> n_items{array_size_sycl};
+                cl::sycl::buffer<cl_float, 1> a_buffer(a_array.data(), n_items);
+                cl::sycl::buffer<cl_float, 1> b_buffer(b_array.data(), n_items);
+                cl::sycl::buffer<cl_float, 1> c_buffer(c_array.data(), n_items);
 
-                h.parallel_for(n_items, [=](cl::sycl::id<1> index) {
-                    c_accessor[index] = a_accessor[index] + coeff * b_accessor[index];
-                }); // end of the kernel
-                tbb::task::resume(sus_point);
-            });
-        });
-    } // end of SYCL code
-}
+                cl::sycl::queue q;
+                q.submit([&](cl::sycl::handler& h) {
+                    auto a_accessor = a_buffer.get_access<sycl_read>(h);
+                    auto b_accessor = b_buffer.get_access<sycl_read>(h);
+                    auto c_accessor = c_buffer.get_access<sycl_write>(h);
+
+                    h.parallel_for(n_items, [=](cl::sycl::id<1> index) {
+                        c_accessor[index] = a_accessor[index] + coeff * b_accessor[index];
+                    }); // end of the kernel
+                }).wait();
+            }
+
+            tbb::task::resume(suspend_point);
+        }) {}
+
+    ~AsyncActivity() {
+        service_thread.join();
+    }
+
+    void submit( float ratio, tbb::task::suspend_point sus_point ) {
+        offload_ratio = ratio;
+        suspend_point = sus_point;
+        submit_flag = true;
+    }
+}; // class AsyncActivity
 
 int main() {
     // init input arrays
@@ -74,6 +97,7 @@ int main() {
     std::size_t n_threads = 4;
     tbb::global_control gc(tbb::global_control::max_allowed_parallelism, n_threads + 1); // One more thread, but sleeping
     tbb::task_group tg;
+    AsyncActivity activity;
 
     // Run CPU part
     tg.run([&]{
@@ -89,7 +113,9 @@ int main() {
     });
 
     // Run GPU part
-    run_sycl_activity(ratio);
+    tbb::task::suspend([&]( tbb::task::suspend_point suspend_point ) {
+        activity.submit(ratio, suspend_point);
+    });
 
     tg.wait();
 
