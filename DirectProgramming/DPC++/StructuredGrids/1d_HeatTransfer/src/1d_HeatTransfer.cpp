70e159fc2b0d4b402a91f07f1f4f5f53e68dca37
Loc Nguyen <loc.q.nguyen@intel.com>
2020-10-02 11:45:54 -0700

    Add comment about dpc_common.hpp .
    Signed-off-by: Loc Nguyen <loc.q.nguyen@intel.com>

--- DirectProgramming/DPC++/StructuredGrids/1d_HeatTransfer/src/1d_HeatTransfer.cpp
+++ DirectProgramming/DPC++/StructuredGrids/1d_HeatTransfer/src/1d_HeatTransfer.cpp
@@ -37,6 +37,8 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+// dpc_common.hpp can be found in the dev-utilities include folder.
+// e.g., $ONEAPI_ROOT/dev-utilities/<version>/include/dpc_common.hpp
 #include "dpc_common.hpp"
 
 using namespace sycl;
