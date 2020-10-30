c158f20ae0387e8658716c79b80cf8fddf422dd8
Gogar, Sunny L <sunny.l.gogar@intel.com>
2020-10-30 11:11:57 -0700

    Adding some headers to enable compilation on windows

--- DirectProgramming/C++/StructuredGrids/iso3dfd_omp_offload/include/iso3dfd.h
+++ DirectProgramming/C++/StructuredGrids/iso3dfd_omp_offload/include/iso3dfd.h
@@ -5,13 +5,14 @@
 // =============================================================
 
 #include <omp.h>
+#include <algorithm>
 #include <chrono>
 #include <cmath>
 #include <cstring>
 #include <ctime>
 #include <fstream>
 #include <iostream>
-#include <algorithm>
+#include <string>
 
 constexpr float dt = 0.002f;
 constexpr float dxyz = 50.0f;
