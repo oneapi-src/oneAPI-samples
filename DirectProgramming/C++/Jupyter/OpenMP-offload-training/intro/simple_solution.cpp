f1fc12bcd07e8283e5aa512535d8a6e2bc55134f
Karl Qi <karl.qi@intel.com>
2020-12-07 00:34:51 -0800

    google style

--- DirectProgramming/C++/Jupyter/OpenMP-offload-training/intro/simple_solution.cpp
+++ DirectProgramming/C++/Jupyter/OpenMP-offload-training/intro/simple_solution.cpp
@@ -4,30 +4,31 @@
 // SPDX-License-Identifier: MIT
 // =============================================================
 #include <omp.h>
+
 #include <iostream>
 
 constexpr int N = 16;
-int main(){
-    int is_cpu = true;
-    int *data = static_cast<int*>(malloc(N * sizeof(int)));
+int main() {
+  int is_cpu = true;
+  int *data = static_cast<int *>(malloc(N * sizeof(int)));
 
-    //Initialization
-    for(int i=0; i<N; i++) data[i] = i;
+  // Initialization
+  for (int i = 0; i < N; i++) data[i] = i;
 
-    // Add the target directive here, including the map clause.
-#pragma omp target map(from:is_cpu) map(tofrom:data[0:N])
-    {
-        is_cpu=omp_is_initial_device();
+  // Add the target directive here, including the map clause.
+#pragma omp target map(from : is_cpu) map(tofrom : data [0:N])
+  {
+    is_cpu = omp_is_initial_device();
 #pragma omp parallel for
-        for (int i=0; i<N; i++) {
-            data[i] *= 2;
-        }
+    for (int i = 0; i < N; i++) {
+      data[i] *= 2;
     }
+  }
 
-    //# Print Output
-    std::cout << "Running on " << (is_cpu ? "CPU" : "GPU") << std::endl;    
-    for(int i=0; i<N; i++) std::cout << data[i] << std::endl;
+  // Print Output
+  std::cout << "Running on " << (is_cpu ? "CPU" : "GPU") << std::endl;
+  for (int i = 0; i < N; i++) std::cout << data[i] << std::endl;
 
-    free(data);
-    return 0;
+  free(data);
+  return 0;
 }
