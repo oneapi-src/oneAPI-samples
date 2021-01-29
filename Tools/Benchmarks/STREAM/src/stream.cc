dea4cad9a86e2efa9913c864b667614147d28f4a
omartiny <olesya.martinyuk@intel.com>
2021-01-29 12:30:11 +0300

    stream code updated

--- Tools/Benchmarks/STREAM/src/stream.cc
+++ Tools/Benchmarks/STREAM/src/stream.cc
@@ -9,6 +9,7 @@
 /* computational kernels coded in C.                                     */
 /*-----------------------------------------------------------------------*/
 /* Copyright 1991-2013: John D. McCalpin                                 */
+/* Copyright 2020-2021: Intel Corporation (oneAPI modifications)         */
 /*-----------------------------------------------------------------------*/
 /* License:                                                              */
 /*  1. You are free to use this program and/or to redistribute           */
@@ -47,6 +48,9 @@
 # include <limits.h>
 # include <sys/time.h>
 
+# include <CL/sycl.hpp>
+# include <iostream>
+
 /*-----------------------------------------------------------------------
  * INSTRUCTIONS:
  *
@@ -62,13 +66,13 @@
  *           Example 1: One Xeon E3 with 8 MB L3 cache
  *               STREAM_ARRAY_SIZE should be >= 4 million, giving
  *               an array size of 30.5 MB and a total memory requirement
- *               of 91.5 MB.  
+ *               of 91.5 MB.
  *           Example 2: Two Xeon E5's with 20 MB L3 cache each (using OpenMP)
  *               STREAM_ARRAY_SIZE should be >= 20 million, giving
  *               an array size of 153 MB and a total memory requirement
- *               of 458 MB.  
+ *               of 458 MB.
  *       (b) The size should be large enough so that the 'timing calibration'
- *           output by the program is at least 20 clock-ticks.  
+ *           output by the program is at least 20 clock-ticks.
  *           Example: most versions of Windows have a 10 millisecond timer
  *               granularity.  20 "ticks" at 10 ms/tic is 200 milliseconds.
  *               If the chip is capable of 10 GB/s, it moves 2 GB in 200 msec.
@@ -77,7 +81,7 @@
  *      Version 5.10 increases the default array size from 2 million
  *          elements to 10 million elements in response to the increasing
  *          size of L3 caches.  The new default size is large enough for caches
- *          up to 20 MB. 
+ *          up to 20 MB.
  *      Version 5.10 changes the loop index variables from "register int"
  *          to "ssize_t", which allows array indices >2^32 (4 billion)
  *          on properly configured 64-bit systems.  Additional compiler options
@@ -113,8 +117,8 @@
 #endif
 
 /*  Users are allowed to modify the "OFFSET" variable, which *may* change the
- *         relative alignment of the arrays (though compilers may change the 
- *         effective offset by making the arrays non-contiguous on some systems). 
+ *         relative alignment of the arrays (though compilers may change the
+ *         effective offset by making the arrays non-contiguous on some systems).
  *      Use of non-zero values for OFFSET can be especially helpful if the
  *         STREAM_ARRAY_SIZE is set to a value close to a large power of 2.
  *      OFFSET can also be set on the compile line without changing the source
@@ -126,7 +130,7 @@
 
 /*
  *	3) Compile the code with optimization.  Many compilers generate
- *       unreasonably bad code before the optimizer tightens things up.  
+ *       unreasonably bad code before the optimizer tightens things up.
  *     If the results are unreasonably good, on the other hand, the
  *       optimizer might be too smart for me!
  *
@@ -137,7 +141,7 @@
  *     To use multiple cores, you need to tell the compiler to obey the OpenMP
  *       directives in the code.  This varies by compiler, but a common example is
  *            gcc -O -fopenmp stream.c -o stream_omp
- *       The environment variable OMP_NUM_THREADS allows runtime control of the 
+ *       The environment variable OMP_NUM_THREADS allows runtime control of the
  *         number of threads/cores used when the resulting "stream_omp" program
  *         is executed.
  *
@@ -146,9 +150,9 @@
  *     to the compile line.
  *     Note that this changes the minimum array sizes required --- see (1) above.
  *
- *     The preprocessor directive "TUNED" does not do much -- it simply causes the 
+ *     The preprocessor directive "TUNED" does not do much -- it simply causes the
  *       code to call separate functions to execute each kernel.  Trivial versions
- *       of these functions are provided, but they are *not* tuned -- they just 
+ *       of these functions are provided, but they are *not* tuned -- they just
  *       provide predefined interfaces to be replaced with tuned code.
  *
  *
@@ -176,15 +180,12 @@
 #define STREAM_TYPE double
 #endif
 
-static STREAM_TYPE	a[STREAM_ARRAY_SIZE+OFFSET],
-			b[STREAM_ARRAY_SIZE+OFFSET],
-			c[STREAM_ARRAY_SIZE+OFFSET];
+STREAM_TYPE *a, *b, *c;
 
 static double	avgtime[4] = {0}, maxtime[4] = {0},
 		mintime[4] = {FLT_MAX,FLT_MAX,FLT_MAX,FLT_MAX};
 
-static char	*label[4] = {"Copy:      ", "Scale:     ",
-    "Add:       ", "Triad:     "};
+static char	label[4][12] = {"Copy:      ", "Scale:     ", "Add:       ", "Triad:     "};
 
 static double	bytes[4] = {
     2 * sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE,
@@ -194,7 +195,7 @@ static double	bytes[4] = {
     };
 
 extern double mysecond();
-extern void checkSTREAMresults();
+extern void checkSTREAMresults ();
 #ifdef TUNED
 extern void tuned_STREAM_Copy();
 extern void tuned_STREAM_Scale(STREAM_TYPE scalar);
@@ -204,10 +205,33 @@ extern void tuned_STREAM_Triad(STREAM_TYPE scalar);
 #ifdef _OPENMP
 extern int omp_get_num_threads();
 #endif
-int
-main()
-    {
-    int			quantum, checktick();
+int checktick();
+
+/* oneAPI modifications: */
+/* This is a global variable to allow the tuned_* implementations
+ * to have the same function signature as the original version. */
+sycl::queue q;
+
+int main(void)
+{
+    /* oneAPI modifications: */
+    /* The default selector will likely choose a GPU, then a CPU.
+     * We print the platform (SYCL implementation) and device information
+     * so the user knows where they are running. */
+    q = sycl::default_selector{};
+    auto d = q.get_device();
+    auto p = d.get_platform();
+    std::cerr << "SYCL Platform: " << p.get_info<sycl::info::platform::name>() << std::endl;
+    std::cerr << "SYCL Device:   " << d.get_info<sycl::info::device::name>() << std::endl;
+
+    /* oneAPI modifications: */
+    /* SYCL 2020 unified shared memory keeps the oneAPI implemenation as similar
+     * to the original version as possible. */
+    a = sycl::malloc_shared<STREAM_TYPE>(STREAM_ARRAY_SIZE, q);
+    b = sycl::malloc_shared<STREAM_TYPE>(STREAM_ARRAY_SIZE, q);
+    c = sycl::malloc_shared<STREAM_TYPE>(STREAM_ARRAY_SIZE, q);
+
+    int			quantum;
     int			BytesPerWord;
     int			k;
     ssize_t		j;
@@ -233,19 +257,19 @@ main()
 #endif
 
     printf("Array size = %llu (elements), Offset = %d (elements)\n" , (unsigned long long) STREAM_ARRAY_SIZE, OFFSET);
-    printf("Memory per array = %.1f MiB (= %.1f GiB).\n", 
+    printf("Memory per array = %.1f MiB (= %.1f GiB).\n",
 	BytesPerWord * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.0),
 	BytesPerWord * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.0/1024.0));
     printf("Total memory required = %.1f MiB (= %.1f GiB).\n",
 	(3.0 * BytesPerWord) * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.),
 	(3.0 * BytesPerWord) * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024./1024.));
     printf("Each kernel will be executed %d times.\n", NTIMES);
-    printf(" The *best* time for each kernel (excluding the first iteration)\n"); 
+    printf(" The *best* time for each kernel (excluding the first iteration)\n");
     printf(" will be used to compute the reported bandwidth.\n");
 
 #ifdef _OPENMP
     printf(HLINE);
-#pragma omp parallel 
+#pragma omp parallel
     {
 #pragma omp master
 	{
@@ -258,7 +282,7 @@ main()
 #ifdef _OPENMP
 	k = 0;
 #pragma omp parallel
-#pragma omp atomic 
+#pragma omp atomic
 		k++;
     printf ("Number of Threads counted = %i\n",k);
 #endif
@@ -273,7 +297,7 @@ main()
 
     printf(HLINE);
 
-    if  ( (quantum = checktick()) >= 1) 
+    if  ( (quantum = checktick()) >= 1)
 	printf("Your clock granularity/precision appears to be "
 	    "%d microseconds.\n", quantum);
     else {
@@ -300,7 +324,7 @@ main()
     printf("For best results, please be sure you know the\n");
     printf("precision of your system timer.\n");
     printf(HLINE);
-    
+
     /*	--- MAIN LOOP --- repeat test cases NTIMES times --- */
 
     scalar = 3.0;
@@ -308,37 +332,49 @@ main()
 	{
 	times[0][k] = mysecond();
 #ifdef TUNED
+        /* oneAPI modifications: */
+        /* SYCL kernels are asynchronous. We synchronize outside of the tuned implemenation. */
         tuned_STREAM_Copy();
+        q.wait();
 #else
 #pragma omp parallel for
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
 	    c[j] = a[j];
 #endif
 	times[0][k] = mysecond() - times[0][k];
-	
+
 	times[1][k] = mysecond();
 #ifdef TUNED
+        /* oneAPI modifications: */
+        /* SYCL kernels are asynchronous. We synchronize outside of the tuned implemenation. */
         tuned_STREAM_Scale(scalar);
+        q.wait();
 #else
 #pragma omp parallel for
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
 	    b[j] = scalar*c[j];
 #endif
 	times[1][k] = mysecond() - times[1][k];
-	
+
 	times[2][k] = mysecond();
 #ifdef TUNED
+        /* oneAPI modifications: */
+        /* SYCL kernels are asynchronous. We synchronize outside of the tuned implemenation. */
         tuned_STREAM_Add();
+        q.wait();
 #else
 #pragma omp parallel for
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
 	    c[j] = a[j]+b[j];
 #endif
 	times[2][k] = mysecond() - times[2][k];
-	
+
 	times[3][k] = mysecond();
 #ifdef TUNED
+        /* oneAPI modifications: */
+        /* SYCL kernels are asynchronous. We synchronize outside of the tuned implemenation. */
         tuned_STREAM_Triad(scalar);
+        q.wait();
 #else
 #pragma omp parallel for
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
@@ -358,7 +394,23 @@ main()
 	    maxtime[j] = MAX(maxtime[j], times[j][k]);
 	    }
 	}
-    
+
+    /* oneAPI modifications: */
+    /* This is here to increase the likelihood that someone running this code will
+     * comply with license term 3b, which requires the disclosure of the use of a
+     * tuned version of the benchmark when publishing results. */
+#ifdef TUNED
+    printf("*****  NOTICE: ******\n");
+    printf("Results based on modified source code or on runs not in\n");
+    printf("accordance with the STREAM Run Rules must be clearly labelled whenever they are published.\n");
+    printf("Examples of proper labelling include:\n");
+    printf("  \"tuned STREAM benchmark results\"\n");
+    printf("  \"based on a variant of the STREAM benchmark code\"\n");
+    printf("Other comparable, clear, and reasonable labelling is acceptable.\n");
+    printf("*****  NOTICE: ******\n");
+    printf(HLINE);
+#endif
+
     printf("Function    Best Rate MB/s  Avg time     Min time     Max time\n");
     for (j=0; j<4; j++) {
 		avgtime[j] = avgtime[j]/(double)(NTIMES-1);
@@ -372,8 +424,15 @@ main()
     printf(HLINE);
 
     /* --- Check Results --- */
-    checkSTREAMresults();
+    checkSTREAMresults ();
     printf(HLINE);
+
+    /* oneAPI modifications: */
+    /* SYCL 2020 unified shared memory keeps the oneAPI implemenation as similar
+     * to the original version as possible. */
+    sycl::free(c, q);
+    sycl::free(b, q);
+    sycl::free(a, q);
 
     return 0;
 }
@@ -430,7 +489,7 @@ double mysecond()
 #ifndef abs
 #define abs(a) ((a) >= 0 ? (a) : -(a))
 #endif
-void checkSTREAMresults ()
+void checkSTREAMresults()
 {
 	STREAM_TYPE aj,bj,cj,scalar;
 	STREAM_TYPE aSumErr,bSumErr,cSumErr;
@@ -548,38 +607,46 @@ void checkSTREAMresults ()
 #endif
 }
 
+/* oneAPI modifications: */
+/* These are straightforward SYCL implementations of the STREAM kernels.
+ * Other implenentations may be better in some cases, e.g. using nd_range
+ * and prescibing the dimensions to be an integer multiple of a device parameter.
+ * Please see SYCL or oneAPI performance tuning documentation if necessary, */
+/* SYCL requires global variables to be captured explicitly, which is why there
+ * are a=a, b=b, c=c below.  This is odd, but consistent with how C++ lambdas work. */
+
 #ifdef TUNED
 /* stubs for "tuned" versions of the kernels */
 void tuned_STREAM_Copy()
 {
-	ssize_t j;
-#pragma omp parallel for
-        for (j=0; j<STREAM_ARRAY_SIZE; j++)
-            c[j] = a[j];
+    q.parallel_for(sycl::range{STREAM_ARRAY_SIZE}, [=,a=a,c=c](sycl::item<1> i) {
+        const auto j = i[0];
+        c[j] = a[j];
+    });
 }
 
 void tuned_STREAM_Scale(STREAM_TYPE scalar)
 {
-	ssize_t j;
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    b[j] = scalar*c[j];
+    q.parallel_for(sycl::range{STREAM_ARRAY_SIZE}, [=,b=b,c=c](sycl::item<1> i) {
+        const auto j = i[0];
+        b[j] = scalar*c[j];
+    });
 }
 
 void tuned_STREAM_Add()
 {
-	ssize_t j;
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    c[j] = a[j]+b[j];
+    q.parallel_for(sycl::range{STREAM_ARRAY_SIZE}, [=,a=a,b=b,c=c](sycl::item<1> i) {
+        const auto j = i[0];
+        c[j] = a[j]+b[j];
+    });
 }
 
 void tuned_STREAM_Triad(STREAM_TYPE scalar)
 {
-	ssize_t j;
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    a[j] = b[j]+scalar*c[j];
+    q.parallel_for(sycl::range{STREAM_ARRAY_SIZE}, [=,a=a,b=b,c=c](sycl::item<1> i) {
+        const auto j = i[0];
+        a[j] = b[j]+scalar*c[j];
+    });
 }
 /* end of stubs for the "tuned" versions of the kernels */
 #endif
