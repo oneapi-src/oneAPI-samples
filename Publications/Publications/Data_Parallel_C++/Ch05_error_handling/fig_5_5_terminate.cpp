a13e229c4b75605d84dae2098dc201f866e1fef5
JoeOster <joseph.e.oster@intel.com>
2021-07-21 08:22:23 -0700

    Update Publication folder

---
+++ Publications/Publications/Data_Parallel_C++/Ch05_error_handling/fig_5_5_terminate.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2020 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello\n";
+
+  std::terminate();
+}
---
+++ Publications/Publications/Data_Parallel_C++/Ch12_device_information/fig_12_2_try_catch.cpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2020 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+
+#include <CL/sycl.hpp>
+#include <iostream>
+using namespace sycl;
+
+int main() {
+
+// BEGIN CODE SNIP
+  auto GPU_is_available = false;
+
+  try {
+    device testForGPU((gpu_selector()));
+    GPU_is_available = true;
+  } catch (exception const& ex) {
+    std::cout << "Caught this SYCL exception: " << ex.what() << std::endl;
+  }
+
+  auto Q = GPU_is_available ? queue(gpu_selector()) : queue(host_selector());
+
+  std::cout << "After checking for a GPU, we are running on:\n "
+    << Q.get_device().get_info<info::device::name>() << "\n";
+
+  // sample output using a system with a GPU:
+  // After checking for a GPU, we are running on:
+  //  Intel(R) Gen9 HD Graphics NEO.
+  // 
+  // sample output using a system with an FPGA accelerator, but no GPU:
+  // Caught this SYCL exception: No device of requested type available.
+  // ...(CL_DEVICE_NOT_FOUND)
+  // After checking for a GPU, we are running on:
+  //  SYCL host device.
+  //
+
+// END CODE SNIP
+  return 0;
+}
+
