b8d95fbf22ec08bed9cc9e25caa0815ff5174ef9
Kannan, Ajaykumar <ajaykumar.kannan@intel.com>
2021-08-04 00:37:10 -0400

    Undo the change to the ac_types headers since they haven't moved yet

--- DirectProgramming/DPC++FPGA/ReferenceDesigns/mvdr_beamforming/src/udp_loopback_test.cpp
+++ DirectProgramming/DPC++FPGA/ReferenceDesigns/mvdr_beamforming/src/udp_loopback_test.cpp
@@ -9,7 +9,7 @@
 #include <sys/mman.h>
 
 #include <CL/sycl.hpp>
-#include <sycl/ext/intel/ac_types/ac_complex.hpp>
+#include <CL/sycl/INTEL/ac_types/ac_complex.hpp>
 #include <sycl/ext/intel/fpga_extensions.hpp>
 
 #include "Tuple.hpp"
