28a1c999173bba05f2c7c0bc6b1df2d77e3d45bf
JoeOster <joseph.e.oster@intel.com>
2021-08-17 10:39:13 -0700

    full update

---
+++ test/Publications/Data_Parallel_C++/Ch05_error_handling/fig_5_6_catch_snip.cpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2020 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+
+#include <CL/sycl.hpp>
+using namespace sycl;
+
+int main() { 
+
+// START CODE SNIP
+
+  try{
+    // Do some SYCL work
+  } catch (sycl::exception &e) {
+    // Do something to output or handle the exceptinon 
+    std::cout << "Caught sync SYCL exception: " << e.what() << "\n";
+    return 1;
+  } 
+
+// END CODE SNIP
+  
+  return 0;
+}
+
