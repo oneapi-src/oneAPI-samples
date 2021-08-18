42feac80e8e144bf9173d6805530ae662b5ba1e1
Seunghyuk Park <seunghyuk.h.park@intel.com>
2021-08-18 11:43:26 -0700

    Implement internal memory usage and zero copy
    Signed-off-by: Seunghyuk Park <seunghyuk.h.park@intel.com>

--- Libraries/oneVPL/dpcpp-blur/src/util.h
+++ Libraries/oneVPL/dpcpp-blur/src/util.h
@@ -197,11 +197,11 @@ void *InitAcceleratorHandle(mfxSession session) {
   return NULL;
 }
 
-void FreeAcceleratorHandle(void *accelHandle) {
 #ifdef LIBVA_SUPPORT
+void FreeAcceleratorHandle(void *accelHandle) {
   vaTerminate((VADisplay)accelHandle);
-#endif
 }
+#endif
 
 // Shows implementation info for Media SDK or oneVPL
 mfxVersion ShowImplInfo(mfxSession session) {
@@ -285,6 +285,7 @@ void ShowImplementationInfo(mfxLoader loader, mfxU32 implnum) {
       printf("unknown\n");
       break;
   }
+  printf("  DeviceID:             %s \n", idesc->Dev.DeviceID);
   MFXDispReleaseImplDescription(loader, idesc);
 
 #if (MFX_VERSION >= 2004)
