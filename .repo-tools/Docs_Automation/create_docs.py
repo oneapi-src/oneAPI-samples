c77028552b5ac7bfc046f0716361adfc499c0e9a
JoeOster <52936608+JoeOster@users.noreply.github.com>
2021-08-23 15:37:52 -0700

    Add doc creation (#621)
    * base commit for automation of docs
    
    * env setups
    
    * changed dir and fixed names
    
    * Create README.md
    
    * Update README.md
    
    * fixed format issues
    
    * fixed format issues
    
    * Update sample.json
    
    * Update sample.json
    
    * Update sample.json
    
    * code format cleanup
    
    * readme updates
    
    * readme updates
    
    * readme updates
    
    * misc updates
    
    * url fix
    
    * removing duplicate guid
    
    * updated guid.json by removing duplicates
    
    * cleanup of old files
    
    * To verbose on cleanup
    
    * Update sample.json
    
    * Update README.md
    
    Minor typo and grammatical fixes.
    
    * Update README.md
    
    * update to fix error
    
    * update guid + misc
    
    Co-authored-by: Mike Tucker <mike.d.b.tucker@intel.com>

---
+++ .repo-tools/Docs_Automation/create_docs.py
@@ -0,0 +1,174 @@
+from json.decoder import JSONDecodeError
+from pathlib import Path
+from datetime import date
+import os
+import json
+from collections import OrderedDict
+today = date.today()
+currentVersion = "2021.4.0"
+fileName = "sample.json"
+fDeviceTargets = "CODESAMPLESLIST.md"
+fChangeLogs = "CHANGELOGS.md"
+freadme = "README.md"
+fguidVer = ".repo-tools\Docs_Automation\guids.json"
+absolute_path = os.getcwd()
+pathLength = len(absolute_path)   #used to modify local path length, so only tree structure is left for crafting the url
+oneAPIURL = 'https://github.com/oneapi-src/oneAPI-samples/tree/master'
+d = today.strftime("%B %d, %Y")
+count = 0
+
+def checkFileExists(checkFile):
+    if os.path.exists(checkFile):
+        os.remove(checkFile)
+    else:
+        print("The " + checkFile + " file does not exist")
+
+def createHeaders(dataContent):
+    nf = open(fDeviceTargets,"w+")
+    nf.write(dataContent['mdDeviceTargetIntrop1'] + dataContent['mdDeviceTargetIntrop2'])
+    nf.close()
+    nf = open(fChangeLogs,"w+")
+    nf.write(dataContent['mdChangeLogHeaderp1'])
+    nf.close()
+    nf = open(freadme,"w+")
+    nf.write("## Introduction\n\n")
+    nf.write(dataContent['mdIntro1'] + "\n" +dataContent['mdIntro2'] + currentVersion + dataContent['mdIntro2.1'] + "\n ### Sample Details\n\n")
+    nf.write(dataContent['mdIntro3'] + dataContent['mdIntro3.1']+dataContent['mdIntro3.2'] + dataContent['mdIntro3.3'] + dataContent['mdIntro3.4'] + dataContent['mdIntro3.5'])
+    nf.write("\n" +dataContent['mdIntro4'] + "\n" +dataContent['mdIntro4.1'] + "\n" +dataContent['mdIntro4.2'])
+    nf.write("\n\n### On Windows Platform\n\n" + dataContent['mdIntro5.1'] + "\n" + dataContent['mdIntro5.2'] + "\n" + dataContent['mdIntro5.3'] + "\n" + dataContent['mdIntro5.4'])
+    nf.write("\n\n## Known Issues or Limitations\n\n## Contributing\n\n" + dataContent['mdIntro6'] + "\n\n" + dataContent['mdIntro7'])
+    nf.close()
+
+def createFooters(count):
+    #setting up Device Targets
+    nf = open(fDeviceTargets,"a")
+    nf.write("Total Samples: " + str(count)+ "\n\n")
+    nf.write(str(dataContent['mdDeviceTargetFooter']) + d)
+    nf.write("\nThis doc was created on: " + d)
+    nf.close()
+    nf = open(freadme,"a+")
+    nf.write(dataContent['mdLicense'])
+    nf.write("\nThis doc was created on: " + d)
+    nf.close()
+    nf = open(fDeviceTargets,"a+")
+    nf.write("\nThis doc was created on: " + d)
+    nf.close()
+    
+def openJson(jsonFile):                 #creating a dictionary
+    jsonData = open(jsonFile)           #open the json file
+    try: 
+        data = json.load(jsonData)      #load json into memory
+    except JSONDecodeError as e:
+        print(str(e)+': ' + jsonFile)
+    return data
+
+def readContent():                                  #readin in strings for use in document creation
+    jsonFile = '.repo-tools\Docs_Automation\content.json'
+    dataContent = openJson(jsonFile)
+    return dataContent
+
+def addVersion(dict_main, dict_version):# After walking thu directories we need to add version to dict_main
+    for key in dict_version.keys():
+        try:
+            if (key in dict_main):
+                dict_tmp=dict_version[key]  #copy to dict_tmp if true 
+                ver=dict_tmp['ver']
+                dict_main[key]['ver']=ver
+
+        except KeyError as e: #not working
+            print(str(e) + ":No Match for guid: ")
+   
+def createChangeLog(count): #sorted but does not include version
+    temp = dict_main.items()
+    sorted_items = sorted(temp, key=lambda key_value: key_value[1]["name"], reverse=False) # sorts by name
+    sorted_by_name = OrderedDict(sorted_items)
+
+    temp=dict_version.items()
+    sorted_items = sorted(temp, key=lambda key_value: key_value[1]["ver"], reverse=True) # sorts by ver
+    sorted_by_ver = OrderedDict(sorted_items)
+
+    nf = open(fChangeLogs,"a+")
+    nf.write(str(count) + dataContent['mdChangeLogHeaderp2'])
+    for key in sorted_by_name.keys():
+        description= str(sorted_by_name[key]['description']) 
+        url= sorted_by_name[key]['url']
+        ver=sorted_by_ver[key]['ver']
+        name=sorted_by_name[key]['name']
+        cat=str(sorted_by_name[key]['categories'])
+
+        if (cat=="['Toolkit/Publication: Data Parallel C++']"):
+            description=description.replace('*','<br>')
+            name ="Pub: Data Parallel C++:](https://www.apress.com/9781484255735)" + "<br><br>[" + name
+        nf.write("|" + ver + "|[" + name+ "](" + url + ")|" + description + "|\n") 
+    nf.close()
+
+def createTtargetedDevices():
+    temp = dict_main.items()
+    sorted_items = sorted(temp, key=lambda key_value: key_value[1]["name"], reverse=False) # sorts by name
+    sorted_by_name = OrderedDict(sorted_items)
+    temp=sorted_by_name.items()
+    nf = open(fDeviceTargets,"a+")
+
+    for key in sorted_by_name.keys():
+        description= str(sorted_by_name[key]['description']) 
+        url= sorted_by_name[key]['url']
+        name=sorted_by_name[key]['name']
+        target= str(sorted_by_name[key]['targetDevice'])
+        cat=str(sorted_by_name[key]['categories'])
+        if (cat=="""['Toolkit/Publication: Data Parallel C++']"""):
+            description=description.replace('*','<br>')
+            name ="Pub: Data Parallel C++:](https://www.apress.com/9781484255735)" + "<br><br>[" + name
+        nf.write("|[" + name+ "](" + url + ")|" + target + "|" + description + "|\n") 
+    nf.close()
+
+def createReadme():
+    temp = dict_main.items()
+    sorted_items = sorted(temp, key=lambda key_value: key_value[1]["name"], reverse=False) # sorts by name
+    sorted_by_name = OrderedDict(sorted_items)
+    nf = open(freadme,"a+")
+    
+    for key in sorted_by_name.keys():
+        description= str(sorted_by_name[key]['description']) 
+        url= sorted_by_name[key]['url']
+        ver=sorted_by_name[key]['ver']
+        name=sorted_by_name[key]['name']
+        cat=str(sorted_by_name[key]['categories'])
+        if (cat=="""['Toolkit/Publication: Data Parallel C++']"""):
+            name ="Pub: Data Parallel C++:](https://www.apress.com/9781484255735)" + "\n\n[" + name
+            description=description.replace('*','<br>')
+        if (ver==currentVersion):
+            nf.write("|" + ver + "|[" + name+ "](" + url + ")|" + description + "|\n") 
+    nf.close()
+   
+#main
+checkFileExists(fDeviceTargets)     #Cleaning up from previous run
+checkFileExists(fChangeLogs)        #Cleaning up from previous run
+checkFileExists(freadme)            #Cleaning up from previous run
+dataContent = readContent()         #read json for data used in creating document header and footers
+createHeaders(dataContent)          #create headers for the various documents being generated
+
+dict_main={}                        
+dict_version = openJson(fguidVer)
+
+for subdir, dirs, files in os.walk('..\\'):
+    for file in files:
+        if (file == fileName):
+            f = os.path.join(subdir, file)
+            # build url
+            fp = os.path.join(subdir)
+            fp = fp.replace('\\','/')                 #char replace \ for /
+            fullURL=oneAPIURL+(str(fp)[17:])  # removed first 17 characters of the path, which is always "../oneAPI-samples/"
+            #end build url
+            data = openJson(f) 
+            dict_main[data['guid']]=data   
+            dict_main[data['guid']]['url'] = fullURL
+            count = count+1
+        # Future - add a search for license file and if none, show a warning
+        # Future - if no sample.json is present then show a warning
+        # future - if no readme.md is present then show a warning
+addVersion(dict_main,dict_version)
+createChangeLog(count)
+createTtargetedDevices()
+createReadme()
+createFooters(count)
+print("Finished")
