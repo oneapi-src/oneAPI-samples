54d447b43067f5a96f3a151d9129794c35b5be7b
mtucker <mike.d.b.tucker@intel.com>
2021-09-08 10:30:58 -0400

    Declare kernel namein global namespace.  Replace INTEL:: with ext::intel:: to eliminate deprication warnings.
    Signed-off-by: mtucker <mike.d.b.tucker@intel.com>

--- DirectProgramming/DPC++/ProjectTemplates/cmake-fpga/src/main.cpp
+++ DirectProgramming/DPC++/ProjectTemplates/cmake-fpga/src/main.cpp
@@ -6,21 +6,24 @@
 
 // located in $ONEAPI_ROOT/compiler/latest/linux/include/sycl/
 #include <CL/sycl.hpp>
-#include <CL/sycl/INTEL/fpga_extensions.hpp>
+#include <sycl/ext/intel/fpga_extensions.hpp>
 #include <cstdlib>
 #include <iostream>
 
 using namespace cl::sycl;
+
+// declare the kernel name as a global to reduce name mangling
+class ExampleKernel;
 
 int main() {
   // create device selector for the device of your interest
   // FPGA_EMULATOR defined in cmake-fpga/src/CMakeLists.txt
 #if defined(FPGA_EMULATOR)
   // DPC++ extension: FPGA emulator selector on systems without FPGA card
-  INTEL::fpga_emulator_selector device_selector;
+  ext::intel::fpga_emulator_selector device_selector;
 #else
   // DPC++ extension: FPGA selector on systems with FPGA card
-  INTEL::fpga_selector device_selector;
+  ext::intel::fpga_selector device_selector;
 #endif
 
   // create a buffer
@@ -29,7 +32,6 @@ int main() {
   buffer A(out_data);
 
   // create a kernel
-  class ExampleKernel;
   queue q{device_selector };
   q.submit([&](handler& h) {
     auto out = A.get_access<access::mode::write>(h);
