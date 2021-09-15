2e8054a36c461869fabacafad93355cb3760338b
JoeOster <joseph.e.oster@intel.com>
2021-09-15 11:31:14 -0700

    update for issues found in validation

--- Publications/DPC++/Ch14_common_parallel_patterns/fig_14_24_local_unpack.cpp
+++ Publications/DPC++/Ch14_common_parallel_patterns/fig_14_24_local_unpack.cpp
@@ -126,9 +126,9 @@ int main() {
 
             // Replace pixels that have converged using an unpack
             // Pixels that haven't converged are not replaced
-            uint32_t index = exclusive_scan(sg, converged, std::plus<>());
+            uint32_t index = exclusive_scan(sg, converged, sycl::ONEAPI::plus<>());
             i = (converged) ? iq + index : i;
-            iq += reduce(sg, converged, std::plus<>());
+            iq += reduce(sg, converged, sycl::ONEAPI::plus<>());
 
             // Reset the iterator variables for the new i
             if (converged) {
