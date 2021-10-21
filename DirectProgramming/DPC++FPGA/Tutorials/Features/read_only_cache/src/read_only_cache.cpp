c80d18d066ccd25303370cabd77270660c55e81d
mohammadfawaz <mohammad.fawaz@intel.com>
2021-10-21 12:50:09 -0700

    s

--- DirectProgramming/DPC++FPGA/Tutorials/Features/read_only_cache/src/read_only_cache.cpp
+++ DirectProgramming/DPC++FPGA/Tutorials/Features/read_only_cache/src/read_only_cache.cpp
@@ -15,51 +15,35 @@ using namespace sycl;
 using namespace sycl::ext::oneapi;
 
 constexpr int kLUTSize = 512;        // Default number of inputs.
-constexpr int kNumInputs = 65536;    // Default number of inputs.
-constexpr int kInitSeed = 42;        // Seed for randomizing data inputs
-constexpr int kNumRuns = 2;          // runs twice to show the impact of cache
+constexpr int kNumInputs = 524288;   // Default number of inputs.
 constexpr double kNs = 1000000000.0; // number of nanoseconds in a second
 
 // Forward declare the kernel name in the global scope.
 // This FPGA best practice reduces name mangling in the optimization reports.
-class SqrtWithoutCaching;
-class SqrtWithCaching;
+class SqrtTest;
 
-void ComputeSqrtWithoutCaching(sycl::queue &q, buffer<float> &sqrt_lut_buf,
-                               buffer<uint32_t> &input_buf,
-                               buffer<float> &output_buf, event &e) {
+void runSqrtTest(sycl::queue &q, const std::vector<float> &sqrt_lut_vec,
+                 std::vector<float> &output_vec, event &e) {
+  range<1> N{output_vec.size()};
+  buffer sqrt_lut_buf(sqrt_lut_vec);
+  buffer output_buf(output_vec.data(), N);
+
   // Enqueue  kernel
   e = q.submit([&](handler &h) {
     // Get accessors to the SYCL buffers
     accessor sqrt_lut(sqrt_lut_buf, h, read_only,
                       accessor_property_list{no_alias});
-    accessor input(input_buf, h, read_write, accessor_property_list{no_alias});
     accessor output(output_buf, h, write_only,
                     accessor_property_list{no_alias, no_init});
 
-    h.single_task<SqrtWithoutCaching>([=]() {
-      for (int i = 0; i < kNumInputs; ++i) {
-        output[i] = sqrt_lut[input[i]];
-      }
-    });
-  });
-}
+    h.single_task<SqrtTest>([=]() {
+      uint16_t index = 0xFFFu;
+      uint16_t bits = 0;
 
-void ComputeSqrtWithCaching(sycl::queue &q, buffer<float> &sqrt_lut_buf,
-                            buffer<uint32_t> &input_buf,
-                            buffer<float> &output_buf, event &e) {
-  // Enqueue  kernel
-  e = q.submit([&](handler &h) {
-    // Get accessors to the SYCL buffers
-    accessor sqrt_lut(sqrt_lut_buf, h, read_write,
-                      accessor_property_list{no_alias});
-    accessor input(input_buf, h, read_write, accessor_property_list{no_alias});
-    accessor output(output_buf, h, write_only,
-                    accessor_property_list{no_alias, no_init});
-
-    h.single_task<SqrtWithCaching>([=]() {
       for (int i = 0; i < kNumInputs; i++) {
-        output[i] = sqrt_lut[input[i]];
+        bits = ((index >> 0) ^ (index >> 3) ^ (index >> 4) ^ (index >> 5)) & 1u;
+        index = (index >> 1) | (bits << 15);
+        output[i] = sqrt_lut[index % kLUTSize];
       }
     });
   });
@@ -70,6 +54,13 @@ int main() {
   event e;
   ulong t1_kernel, t2_kernel;
   double time_kernel;
+
+  // Create input and output vectors
+  std::vector<float> sqrt_lut_vec(kLUTSize);
+  std::vector<float> output_vec(kNumInputs);
+  for (int i = 0; i < kLUTSize; ++i) {
+    sqrt_lut_vec[i] = sqrt(i);
+  }
 
 // Create queue, get platform and device
 #if defined(FPGA_EMULATOR)
@@ -97,94 +88,16 @@ int main() {
     std::cout << "\nSQRT LUT Size: " << kLUTSize << "\n";
     std::cout << "Number of inputs: " << kNumInputs << "\n";
 
-    // Create input and output buffers
-    auto sqrt_lut_buf = buffer<float>(range<1>(kLUTSize));
-    auto input_buf = buffer<uint32_t>(range<1>(kNumInputs));
-    auto output_buf = buffer<float>(range<1>(kNumInputs));
+    runSqrtTest(q, sqrt_lut_vec, output_vec, e);
 
-    srand(kInitSeed);
+    // Wait for kernels to finish
+    q.wait();
 
-    // Compute the reference solution
-    float gold[kNumInputs];
+    // Compute kernel execution time
+    t1_kernel = e.get_profiling_info<info::event_profiling::command_start>();
+    t2_kernel = e.get_profiling_info<info::event_profiling::command_end>();
+    time_kernel = (t2_kernel - t1_kernel) / kNs;
 
-    {
-      // Get host-side accessors to the SYCL buffers
-      host_accessor sqrt_lut_host(sqrt_lut_buf, write_only);
-      host_accessor input_host(input_buf, write_only);
-      // Initialize random input
-      for (int i = 0; i < kLUTSize; ++i) {
-        sqrt_lut_host[i] = sqrt(i);
-      }
-
-      for (int i = 0; i < kNumInputs; ++i) {
-        input_host[i] = rand() % kLUTSize;
-      }
-
-      for (int i = 0; i < kNumInputs; ++i) {
-        gold[i] = sqrt_lut_host[input_host[i]];
-      }
-    }
-
-    // Host accessor is now out-of-scope and is destructed. This is required
-    // in order to unblock the kernel's subsequent accessor to the same buffer.
-
-    for (int i = 0; i < kNumRuns; i++) {
-      switch (i) {
-      case 0: {
-        std::cout << "Beginning run with caching disabled.\n\n";
-        ComputeSqrtWithoutCaching(q, sqrt_lut_buf, input_buf, output_buf, e);
-        break;
-      }
-      case 1: {
-        std::cout << "Beginning run with caching enabled.\n\n";
-        ComputeSqrtWithCaching(q, sqrt_lut_buf, input_buf, output_buf, e);
-        break;
-      }
-      default: {
-        ComputeSqrtWithoutCaching(q, sqrt_lut_buf, input_buf, output_buf, e);
-      }
-      }
-
-      // Wait for kernels to finish
-      q.wait();
-
-      // Compute kernel execution time
-      t1_kernel = e.get_profiling_info<info::event_profiling::command_start>();
-      t2_kernel = e.get_profiling_info<info::event_profiling::command_end>();
-      time_kernel = (t2_kernel - t1_kernel) / kNs;
-
-      // Get accessor to output buffer. Accessing the buffer at this point in
-      // the code will block on kernel completion.
-      host_accessor output_host(output_buf, read_only);
-
-      // Verify output and print pass/fail
-      bool passed = true;
-      int num_errors = 0;
-      for (int b = 0; b < kNumInputs; b++) {
-        if (num_errors < 10 && output_host[b] != gold[b]) {
-          passed = false;
-          std::cout << " (mismatch, expected " << gold[b] << ")\n";
-          num_errors++;
-        }
-      }
-
-      if (passed) {
-        std::cout << "Verification PASSED\n\n";
-
-        // Report host execution time and throughput
-        std::cout.setf(std::ios::fixed);
-        double N_MB = (kNumInputs * sizeof(uint32_t)) /
-                      (1024 * 1024); // Input size in MB
-
-        // Report kernel execution time and throughput
-        std::cout << "Kernel execution time: " << time_kernel << " seconds\n";
-        std::cout << "Kernel throughput " << (i == 0 ? "without" : "with")
-                  << " caching: " << N_MB / time_kernel << " MB/s\n\n";
-      } else {
-        std::cout << "Verification FAILED\n";
-        return 1;
-      }
-    }
   } catch (sycl::exception const &e) {
     // Catches exceptions in the host code
     std::cerr << "Caught a SYCL host exception:\n" << e.what() << "\n";
@@ -198,6 +111,43 @@ int main() {
                    "-DFPGA_EMULATOR.\n";
     }
     std::terminate();
+  }
+
+  // Compute the reference solution
+  uint16_t index = 0xFFFu;
+  uint16_t bits = 0;
+  float gold[kNumInputs];
+  for (int i = 0; i < kNumInputs; ++i) {
+    bits = ((index >> 0) ^ (index >> 3) ^ (index >> 4) ^ (index >> 5)) & 1u;
+    index = (index >> 1) | (bits << 15);
+    gold[i] = sqrt_lut_vec[index % kLUTSize];
+  }
+
+  // Verify output and print pass/fail
+  bool passed = true;
+  int num_errors = 0;
+  for (int b = 0; b < kNumInputs; b++) {
+    if (num_errors < 10 && output_vec[b] != gold[b]) {
+      passed = false;
+      std::cout << " (mismatch, expected " << gold[b] << ")\n";
+      num_errors++;
+    }
+  }
+
+  if (passed) {
+    std::cout << "Verification PASSED\n\n";
+
+    // Report host execution time and throughput
+    std::cout.setf(std::ios::fixed);
+    double N_MB =
+        (kNumInputs * sizeof(uint32_t)) / (1024 * 1024); // Input size in MB
+
+    // Report kernel execution time and throughput
+    std::cout << "Kernel execution time: " << time_kernel << " seconds\n";
+    std::cout << "Kernel throughput " << N_MB / time_kernel << " MB/s\n\n";
+  } else {
+    std::cout << "Verification FAILED\n";
+    return 1;
   }
   return 0;
 }
