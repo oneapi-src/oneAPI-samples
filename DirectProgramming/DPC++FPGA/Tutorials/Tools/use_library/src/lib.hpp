0bb8eb74543fe6daece52d2b3436dbf8e8ecdaa6
Kannan, Ajaykumar <ajaykumar.kannan@intel.com>
2021-10-22 15:08:55 -0400

    Re-add original use_library tutorial

---
+++ DirectProgramming/DPC++FPGA/Tutorials/Tools/use_library/src/lib.hpp
@@ -0,0 +1,9 @@
+//==============================================================
+// Copyright Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+// =============================================================
+SYCL_EXTERNAL float HlsSqrtf(float);
+SYCL_EXTERNAL extern "C" float OclSquare(float);
+SYCL_EXTERNAL float SyclSquare(float);
+SYCL_EXTERNAL extern "C" unsigned RtlByteswap(unsigned x);
