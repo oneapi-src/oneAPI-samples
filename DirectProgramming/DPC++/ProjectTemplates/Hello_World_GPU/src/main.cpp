c8fd7c9df7ded290125e02efe615304d8086092e
iochocki <igor.ochocki@intel.com>
2021-12-02 23:23:26 +0100

    Remove deprecated code

--- DirectProgramming/DPC++/ProjectTemplates/Hello_World_GPU/src/main.cpp
+++ DirectProgramming/DPC++/ProjectTemplates/Hello_World_GPU/src/main.cpp
@@ -9,9 +9,6 @@
 
 using namespace cl::sycl;
 
-// declare the kernel name as a global to reduce name mangling
-class ExampleKernel;
-
 int main() {
   
   // GPU Device selector
@@ -26,7 +23,7 @@ int main() {
   queue q{ device_selector };
   q.submit([&](handler& h) {
     auto out = A.get_access<access::mode::write>(h);
-    h.parallel_for<ExampleKernel>(R, [=](id<1> idx) { out[idx] = idx[0]; }); 
+    h.parallel_for(R, [=](id<1> idx) { out[idx] = idx[0]; }); 
   });
   
   // Consume result
--- DirectProgramming/DPC++/ProjectTemplates/cmake-gpu/src/main.cpp
+++ DirectProgramming/DPC++/ProjectTemplates/cmake-gpu/src/main.cpp
@@ -11,9 +11,6 @@
 
 using namespace cl::sycl;
 
-// declare the kernel name as a global to reduce name mangling
-class ExampleKernel;
-
 int main() {
   // create GPU device selector
   gpu_selector device_selector;
@@ -27,7 +24,7 @@ int main() {
   queue q{device_selector };
   q.submit([&](handler& h) {
     auto out = A.get_access<access::mode::write>(h);
-    h.parallel_for<ExampleKernel>(R, [=](id<1> idx) { out[idx] = idx[0]; });
+    h.parallel_for(R, [=](id<1> idx) { out[idx] = idx[0]; });
   });
 
   // consume result
