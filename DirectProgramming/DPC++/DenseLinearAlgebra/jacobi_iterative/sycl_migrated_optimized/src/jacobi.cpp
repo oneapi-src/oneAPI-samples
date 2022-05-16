d3fe7ab2d3c8f3c0f908a1e914c8160d8e1dffb9
PradeepPotteti <Potteti.pradeep@gmail.com>
2022-05-16 05:27:04 -0700

    updated sycl_migrated_optimized and sycl_migrated with group_barrier and namespace changes

--- DirectProgramming/DPC++/DenseLinearAlgebra/jacobi_iterative/sycl_migrated_optimized/src/jacobi.cpp
+++ DirectProgramming/DPC++/DenseLinearAlgebra/jacobi_iterative/sycl_migrated_optimized/src/jacobi.cpp
@@ -38,7 +38,6 @@
 #include "jacobi.h"
 
 using namespace sycl;
-using namespace sycl::ext::oneapi;
 // 8 Rows of square-matrix A processed by each CTA.
 
 #define ROWS_PER_CTA 32
@@ -64,7 +63,7 @@ static void JacobiMethod(const float *A, const double *b,
     }
   }
 
-  item_ct1.barrier();
+  sycl::group_barrier(item_ct1.get_group());
 
   sub_group tile_sg = item_ct1.get_sub_group();
 
@@ -86,7 +85,7 @@ static void JacobiMethod(const float *A, const double *b,
     }
   }
 
-  item_ct1.barrier();
+  sycl::group_barrier(item_ct1.get_group());
 
   if (item_ct1.get_local_id(2) < ROWS_PER_CTA) {
     sub_group tile_sg = item_ct1.get_sub_group();
@@ -141,7 +140,7 @@ static void finalError(double *x, double *d_sum, nd_item<3> item_ct1,
     sg_Sum[item_ct1.get_local_id(2) / tile_sg.get_local_range().get(0)] = sum;
   }
 
-  item_ct1.barrier();
+  sycl::group_barrier(item_ct1.get_group());
 
   double blockSum = 0.0;
   if (item_ct1.get_local_id(2) <
