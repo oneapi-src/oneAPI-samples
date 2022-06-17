22e70981bc05526285e1d17688158c23de7885a0
Anuja Ketan Tole <46543365+aktole@users.noreply.github.com>
2022-06-16 19:03:42 -0700

    New FPGA code sample board_test
    Signed-off-by: Anuja Ketan Tole <46543365+aktole@users.noreply.github.com>

---
+++ DirectProgramming/DPC++FPGA/Tutorials/Tools/board_test/src/helper.hpp
@@ -0,0 +1,203 @@
+// Header file to accompany board_test
+#include <CL/sycl.hpp>
+using namespace sycl;
+
+constexpr size_t kKB = 1024;
+constexpr size_t kMB = 1024 * 1024;
+constexpr size_t kGB = 1024 * 1024 * 1024;
+constexpr size_t kRandomSeed = 1009;
+std::string kBinaryName = "board_test.fpga";
+
+#ifdef __linux__
+#define _popen popen
+#define _pclose pclose
+#endif
+
+//////////////////////////////////
+// **** PrintHelp function **** //
+//////////////////////////////////
+
+// Input:
+// int details - Selection between long help or short help
+// Returns:
+// None
+
+// The function does the following task:
+// Prints short help with usage infomation and a longer help with details about
+// each test
+
+void PrintHelp(int details) {
+  if (!details) {
+    std::cout << "\n*** Board_test usage information ***\n"
+              << "Command to run board_test using generated binary:\n"
+              << "  > To run all tests (default): ./board_test.fpga\n"
+              << "  > To run a specific test (see list below); pass the test "
+              << "number as argument to \"-test\" option: ./board_test.fpga "
+              << "-test=<test_number>\n"
+              << "  > To see more details on what each test does: "
+              << "./board_test.fpga -help\n"
+              << "The tests are:\n"
+              << "  1. Host Speed and Host Read Write Test\n"
+              << "  2. Kernel Clock Frequency Test\n"
+              << "  3. Kernel Launch Test\n"
+              << "  4. Kernel Latency Measurement\n"
+              << "  5. Kernel-to-Memory Read Write Test\n"
+              << "  6. Kernel-to-Memory Bandwidth Test\n"
+              << "Note: Kernel Clock Frequency is run along with all tests "
+              << "except 1 (Host Speed and Host Read Write test)\n\n";
+  } else {
+    std::cout
+        << "*** Board_test test details ***\n"
+        << "Command to run board_test using generated binary:\n"
+        << "  > To run all tests (default): ./board_test.fpga\n"
+        << "  > To run a specific test (see list below); pass the test number "
+        << "as argument to \"-test\" option: ./board_test.fpga "
+        << "-test=<test_number>\n"
+        << "The tests are:\n\n"
+        << "  * 1. Host Speed and Host Read Write Test *\n"
+        << "    Host Speed and Host Read Write test check the host to device "
+        << "interface\n"
+        << "    Host Speed test measures the host to device global memory "
+        << "read, write as well as read-write bandwidth and reports it\n"
+        << "    Host Read Write Test writes does unaligned memory to unaligned "
+        << "device memory writes as well as reads from device to unaligned "
+        << "host memory\n\n"
+        << "  * 2. Kernel Clock Frequency Test *\n"
+        << "    Kernel Clock Frequency Test measures the kernel clock "
+        << "frequency of the bitstream running on the FPGA and compares this "
+        << "to the Quartus compiled frequency for the kernel.\n\n"
+        << "  * 3. Kernel Launch Test *\n"
+        << "    Kernel Launch test checks if the kernel launched and executed "
+        << "successfully. This is done by launching a sender kernel that "
+        << "writes a value to a pipe, this pipe is read by the receiver "
+        << "kernel, which completes if the correct value is read.\n"
+        << "    This test will hang if the receiver kernel does not receive "
+        << "the correct value\n\n"
+        << "  * 4. Kernel Latency Measurement *\n"
+        << "    This test measures the round trip kernel latency by launching "
+        << "a no-operation kernel\n\n"
+        << "  * 5. Kernel-to-Memory Read Write Test *\n"
+        << "    Kernel-to-Memory Read Write test checks kernel to device "
+        << "global memory interface. The test writes data to the entire device "
+        << "global memory from host; the kernel then reads -> modifies and "
+        << "writes the data back to the device global memory."
+        << "    The host reads the modified data back and verifies the read "
+        << "back values match expected value\n"
+        << "  * 6. Kernel-to-Memory Bandwidth Test *\n"
+        << "    Kernel-to-Memory Bandwidth test measures the kernel to device "
+        << "global memory bandwidth and compares this with the theoretical "
+        << "bandwidth defined in board_spec.xml file in the oneAPI shim/BSP.\n\n"
+        << "    Note: This test assumes that design was compiled with "
+        << "-Xsno-interleaving option\n\n"
+        << "Please use the commands shown at the beginning of this help to run "
+        << "all or one of the above tests\n\n";
+  }
+}  // End of PrintHelp
+
+//////////////////////////////////////////////
+// **** SyclGetQSubExecTimeNs function **** //
+//////////////////////////////////////////////
+
+// Input:
+// event e - SYCL event with profiling information
+// Returns:
+// Difference in time from command submission to command end (in nanoseconds)
+
+// The function does the following task:
+// Gets profiling information from a SYCL event and
+// returns execution time for a given SYCL event from a queue
+
+unsigned long SyclGetQSubExecTimeNs(event e) {
+  unsigned long submit_time =
+      e.get_profiling_info<info::event_profiling::command_submit>();
+  unsigned long end_time =
+      e.get_profiling_info<info::event_profiling::command_end>();
+  return (end_time - submit_time);
+}  // End of SyclGetQSubExecTimeNs
+
+/////////////////////////////////////////////
+// **** SyclGetQStExecTimeNs function **** //
+/////////////////////////////////////////////
+
+// Input:
+// event e - Sycl event with profiling information
+// Returns:
+// Difference in time from command start to command end (in nanoseconds)
+
+// The function does the following task:
+// Gets profiling information from a Sycl event and
+// returns execution time for a given SYCL event from a queue
+
+unsigned long SyclGetQStExecTimeNs(event e) {
+  unsigned long start_time =
+      e.get_profiling_info<info::event_profiling::command_start>();
+  unsigned long end_time =
+      e.get_profiling_info<info::event_profiling::command_end>();
+  return (end_time - start_time);
+}  // End of SyclGetQStExecTimeNs
+
+///////////////////////////////////////////
+// **** SyclGetTotalTimeNs function **** //
+///////////////////////////////////////////
+
+// Input:
+// event first_evt - Sycl event with profiling information
+// event last_evt - another Sycl event with profiling information
+// Returns:
+// Difference in time from command submission of first event to command end of
+// last event (in nanoseconds)
+
+// The function does the following task:
+// Gets profiling information from two different Sycl events and
+// returns the total execution time for all events between first and last
+
+unsigned long SyclGetTotalTimeNs(event first_evt, event last_evt) {
+  unsigned long first_evt_submit =
+      first_evt.get_profiling_info<info::event_profiling::command_submit>();
+  unsigned long last_evt_end =
+      last_evt.get_profiling_info<info::event_profiling::command_end>();
+  return (last_evt_end - first_evt_submit);
+}  // End of SyclGetTotalTimeNs
+
+/////////////////////////////////////////
+// **** InitializeVector function **** //
+/////////////////////////////////////////
+
+// Inputs:
+// 1. unsigned *vector - pointer to host memory that has to be initialized
+// (allocated in calling function)
+// 2. size_t size - number of elements to initialize
+// 3. size_t offset - value to use for initialization
+// Returns:
+// None
+
+// The function does the following task:
+// Initializes "size" number of elements in memory pointed
+// to with "offset + i", where i is incremented by loop controlled by "size"
+
+void InitializeVector(unsigned *vector, size_t size, size_t offset) {
+  for (size_t i = 0; i < size; ++i) {
+    vector[i] = offset + i;
+  }
+}
+
+/////////////////////////////////////////
+// **** InitializeVector function **** //
+/////////////////////////////////////////
+
+// Inputs:
+// 1. unsigned *vector - pointer to host memory that has to be initialized
+// (allocated in calling function)
+// 2. size_t size - number of elements to initialize
+// Returns:
+// None
+
+// The function does the following task:
+// Initializes "size" number of elements in memory pointed
+// to with random values (output of rand() function)
+
+void InitializeVector(unsigned *vector, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    vector[i] = rand();
+  }
+}
---
+++ DirectProgramming/DPC++FPGA/Tutorials/Tools/board_test/src/host_speed.hpp
@@ -0,0 +1,211 @@
+// Header file to accompany hostspeed tests
+#include <CL/sycl.hpp>
+#include <iomanip>
+#include <iostream>
+
+#include "helper.hpp"
+
+using namespace sycl;
+
+// struct used in ReadSpeed & WriteSpeed functions to store transfer speeds
+struct Speed {
+  float fastest;
+  float slowest;
+  float average;
+  float total;
+};
+
+///////////////////////////////////
+// **** WriteSpeed function **** //
+///////////////////////////////////
+
+// Inputs:
+// 1. queue &q - queue to submit operation
+// 2. buffer<char,1> &device_buffer - device buffer to write to (created in
+// calling function)
+// 3. char *hostbuf_wr - host memory that contains input data to be written to
+// device (allocated in calling function)
+// 4. size_t block_bytes - size of 1 transfer to device (in bytes)
+// 5. size_t total_bytes - total number of bytes to transfer
+// Returns:
+// struct Speed with the transfer times
+
+// The function does the following tasks:
+// 1. Write data to device in multiple transfers as total_bytes > block_bytes
+// (i.e. size of 1 transfer)
+// 2. Calculate bandwidth based on measured time for each transfer
+
+struct Speed WriteSpeed(queue &q, buffer<char, 1> &device_buffer,
+                        char *hostbuf_wr, size_t block_bytes,
+                        size_t total_bytes) {
+  // Total number of iterations to transfer all bytes in block_bytes sizes
+  size_t num_xfers = total_bytes / block_bytes;
+
+  assert(num_xfers > 0);
+
+  // Sycl event for each transfer
+  event evt[num_xfers];
+
+  // **** Write to device **** //
+
+  for (size_t i = 0; i < num_xfers; i++) {
+    // Submit copy operation (explicit copy from host to device)
+    evt[i] = q.submit([&](handler &h) {
+      // Range of buffer that needs to accessed
+      auto buf_range = block_bytes / sizeof(char);
+      // offset starts at 0 - incremented by transfer size each iteration (i.e.
+      // block_bytes)
+      auto buf_offset = (i * block_bytes) / sizeof(char);
+      // Accessor to access range of device buffer at buf_offset
+      accessor<char, 1, access::mode::write> mem(device_buffer, h, buf_range,
+                                                 buf_offset);
+      h.copy(&hostbuf_wr[buf_offset], mem);
+    });
+  }
+  // Wait for copy to complete
+  q.wait();
+
+  // **** Get the time for each transfer from Sycl event array **** //
+
+  struct Speed speed_wr;
+  speed_wr.average = 0.0f;
+  speed_wr.fastest = 0.0f;
+  speed_wr.slowest = 1.0e7f;
+
+  for (size_t i = 0; i < num_xfers; i++) {
+    float time_ns = SyclGetQSubExecTimeNs(evt[i]);
+    float speed_MBps = ((float)block_bytes / kMB) / ((float)time_ns * 1e-9);
+
+    if (speed_MBps > speed_wr.fastest) speed_wr.fastest = speed_MBps;
+    if (speed_MBps < speed_wr.slowest) speed_wr.slowest = speed_MBps;
+
+    speed_wr.average += time_ns;
+  }
+
+  // Average write bandwidth
+  speed_wr.average =
+      ((float)total_bytes / kMB) / ((float)speed_wr.average * 1e-9);
+  speed_wr.total =
+      ((float)total_bytes / kMB) /
+      ((float)SyclGetTotalTimeNs(evt[0], evt[num_xfers - 1]) * 1e-9);
+
+  return speed_wr;
+
+}  // End of WriteSpeed
+
+//////////////////////////////////
+// **** ReadSpeed function **** //
+//////////////////////////////////
+
+// Inputs:
+// 1. queue &q - queue to submit operation
+// 2. buffer<char,1> &device_buffer - device buffer to read from (created in
+// calling function)
+// 3. char *hostbuf_rd - pointer to host memory to store data that is read from
+// device (allocated in calling function)
+// 4. size_t block_bytes - size of 1 transfer to device (in bytes)
+// 5. size_t total_bytes - total number of bytes to transfer
+// Returns:
+// struct Speed with the transfer times
+
+// The function does the following tasks:
+// 1. Read data from device in multiple transfers as total_bytes > block_bytes
+// (i.e. size of 1 transfer)
+// 2. Calculate bandwidth based on measured time for each transfer
+
+struct Speed ReadSpeed(queue &q, buffer<char, 1> &device_buffer,
+                       char *hostbuf_rd, size_t block_bytes,
+                       size_t total_bytes) {
+  // Total number of iterations to transfer all bytes in block_bytes sizes
+  size_t num_xfers = total_bytes / block_bytes;
+
+  assert(num_xfers > 0);
+
+  // Sycl event for each transfer
+  event evt[num_xfers];
+
+  // **** Read from device **** //
+
+  for (size_t i = 0; i < num_xfers; i++) {
+    // Submit copy operation (explicit copy from device to host)
+    evt[i] = q.submit([&](handler &h) {
+      // Range of buffer that needs to accessed
+      auto buf_range = block_bytes / sizeof(char);
+      // offset starts at 0 - incremented by transfer size each iteration (i.e
+      // block_bytes)
+      auto buf_offset = (i * block_bytes) / sizeof(char);
+      // Accessor to access range of device buffer at buf_offset
+      accessor<char, 1, access::mode::read> mem(device_buffer, h, buf_range,
+                                                buf_offset);
+      h.copy(mem, &hostbuf_rd[buf_offset]);
+    });
+  }
+  // Wait for copy to complete
+  q.wait();
+
+  // **** Get the time for each transfer from Sycl event array **** //
+
+  struct Speed speed_rd;
+  speed_rd.average = 0.0f;
+  speed_rd.fastest = 0.0f;
+  speed_rd.slowest = 1.0e7f;
+
+  for (size_t i = 0; i < num_xfers; i++) {
+    float time_ns = SyclGetQSubExecTimeNs(evt[i]);
+    float speed_MBps = ((float)block_bytes / kMB) / ((float)time_ns * 1e-9);
+
+    if (speed_MBps > speed_rd.fastest) speed_rd.fastest = speed_MBps;
+    if (speed_MBps < speed_rd.slowest) speed_rd.slowest = speed_MBps;
+
+    speed_rd.average += time_ns;
+  }
+
+  // Average read bandwidth
+  speed_rd.average =
+      ((float)total_bytes / kMB) / ((float)speed_rd.average * 1e-9);
+  speed_rd.total =
+      ((float)total_bytes / kMB) /
+      ((float)SyclGetTotalTimeNs(evt[0], evt[num_xfers - 1]) * 1e-9);
+
+  return speed_rd;
+
+}  // End of ReadSpeed
+
+/////////////////////////////////////
+// **** CheckResults function **** //
+/////////////////////////////////////
+
+// Inputs:
+// 1. char *hostbuf_rd - pointer to host memory with data read back from device
+// (allocated in calling function)
+// 2. char *hostbuf_wr - pointer to host memory with input data written to
+// device (allocated in calling function)
+// 3. maxchars - size of comparison
+// Returns:
+// true if verification is successfull
+
+// The function does the following tasks:
+// 1. Compare maxchars elements of hostbuf_rd to hostbuf_wr
+// 2. Return false if a mismatch is found
+// 3. Return true if all values read back from device match the values that were
+// written
+
+bool CheckResults(char *hostbuf_rd, char *hostbuf_wr, size_t maxchars) {
+  bool result = true;
+  int prints = 0;
+
+  for (auto j = 0; j < maxchars; j++) {
+    if (hostbuf_rd[j] != hostbuf_wr[j]) {
+      if (prints++ < 512) {  // only print 512 errors
+        std::cerr << "Error! Mismatch at element " << j << ":" << std::setw(8)
+                  << std::hex << std::showbase << hostbuf_rd[j]
+                  << " != " << hostbuf_wr[j] << ", xor = " << std::setfill('0')
+                  << (hostbuf_rd[j] ^ hostbuf_wr[j]) << "\n";
+      }
+      result = false;
+    }
+  }
+
+  return result;
+
+}  // End of CheckResults
