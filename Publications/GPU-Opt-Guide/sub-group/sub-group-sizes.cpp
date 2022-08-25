aa145a77619ef613de3c37b88c356f3fabc24de2
Peng Tu <peng.tu1@gmail.com>
2022-08-25 09:21:12 -0700

    Add GPU-Opt-Guide examples to Publications folder (#1040)
    * Add GPU Optimization Guide Examples
    
    Signed-off-by: Tu, Peng <peng.tu@intel.com>
    
    * Edit the folder name to oneAPI-samples/Publications/GPU-Opt-Guide
    
    Signed-off-by: Tu, Peng <peng.tu@intel.com>

---
+++ Publications/GPU-Opt-Guide/sub-group/sub-group-sizes.cpp
@@ -0,0 +1,23 @@
+//==============================================================
+// Copyright © 2022 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+// =============================================================
+#include <CL/sycl.hpp>
+#include <iostream>
+
+int main(void) {
+  sycl::queue q{sycl::gpu_selector()};
+  std::cout << "Device: " << q.get_device().get_info<sycl::info::device::name>()
+            << std::endl;
+  // Snippet begin
+  std::cout << "Sub-group Sizes: ";
+  for (const auto &s :
+       q.get_device().get_info<sycl::info::device::sub_group_sizes>()) {
+    std::cout << s << " ";
+  }
+  std::cout << std::endl;
+  // Snippet end
+
+  return 0;
+}
