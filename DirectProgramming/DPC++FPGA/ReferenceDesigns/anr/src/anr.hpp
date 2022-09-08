4ed64693cc72ccffb4ed91f68352327308f47654
Yohann Uguen <yohann.uguen@intel.com>
2022-09-08 08:19:48 +0100

    using updated ac_types includes
    Signed-off-by: Yohann Uguen <yohann.uguen@intel.com>

--- DirectProgramming/DPC++FPGA/ReferenceDesigns/anr/src/anr.hpp
+++ DirectProgramming/DPC++FPGA/ReferenceDesigns/anr/src/anr.hpp
@@ -9,7 +9,7 @@
 
 #include <CL/sycl.hpp>
 #include <sycl/ext/intel/fpga_extensions.hpp>
-#include <CL/sycl/INTEL/ac_types/ac_int.hpp>
+#include <sycl/ext/intel/ac_types/ac_int.hpp>
 #include <limits>
 #include <type_traits>
 #include <utility>
