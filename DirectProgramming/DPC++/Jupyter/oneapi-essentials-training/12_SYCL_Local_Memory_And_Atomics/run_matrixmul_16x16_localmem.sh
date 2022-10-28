f481ae99126870ca1ac72c20aba82f7164ae611e
Piotr Kanclerz <pkanclerz997@gmail.com>
2022-10-28 11:45:14 +0200

    Fixes for vscode metadata in Notebooks

--- DirectProgramming/DPC++/Jupyter/oneapi-essentials-training/12_SYCL_Local_Memory_And_Atomics/run_matrixmul_16x16_localmem.sh
+++ DirectProgramming/DPC++/Jupyter/oneapi-essentials-training/12_SYCL_Local_Memory_And_Atomics/run_matrixmul_16x16_localmem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
-/bin/echo "##" $(whoami) is compiling SYCL_Essentials Module12 -- SYCL tomics Local Memory - 5 of 5 matrixmul_16x16_localmem.cpp
+/bin/echo "##" $(whoami) is compiling SYCL_Essentials Module12 -- SYCL Atomics Local Memory - 5 of 5 matrixmul_16x16_localmem.cpp
 dpcpp lab/matrixmul_16x16_localmem.cpp 
 if [ $? -eq 0 ]; then ./a.out; fi
 
