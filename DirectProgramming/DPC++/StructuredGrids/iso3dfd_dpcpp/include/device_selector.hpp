d414ea7882a8ea394332baf4e6bc5f0947e08750
Kanclerz <piotr.kanclerz@intel.com>
2022-11-02 16:32:16 +0100

    StructuredGrids/iso3dfd

--- DirectProgramming/DPC++/StructuredGrids/iso3dfd_dpcpp/include/device_selector.hpp
+++ DirectProgramming/DPC++/StructuredGrids/iso3dfd_dpcpp/include/device_selector.hpp
@@ -10,7 +10,7 @@
 #include <cstring>
 #include <iostream>
 #include <string>
-#include "CL/sycl.hpp"
+#include <sycl/sycl.hpp>
 
 // This is the class provided to SYCL runtime by the application to decide
 // on which device to run, or whether to run at all.
@@ -20,14 +20,14 @@
 // which '()' returned the highest number, is selected. If a negative number
 // was returned for all devices, then the selection process will cause an
 // exception.
-class MyDeviceSelector : public sycl::device_selector {
+class MyDeviceSelector {
  public:
   MyDeviceSelector(const std::string &p) : pattern(p) {
     // std::cout << "Looking for \"" << p << "\" devices" << std::endl;
   }
 
   // This is the function which gives a "rating" to devices.
-  virtual int operator()(const sycl::device &device) const override {
+  virtual int operator()(const sycl::device &device) const {
     // The template parameter to device.get_info can be a variety of properties
     // defined by the SYCL spec's sycl::info:: enum. Properties may have
     // different types. Here we query name which is a string.
