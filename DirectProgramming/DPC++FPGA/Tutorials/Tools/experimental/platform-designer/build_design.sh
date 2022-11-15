04cd06839047a35c2e6aeb7d43d1ae80e9377a7c
White, Paul <paul.white@intel.com>
2022-11-15 06:53:58 -0800

    create script to build design

--- DirectProgramming/DPC++FPGA/Tutorials/Tools/experimental/platform-designer/build_design.sh
+++ DirectProgramming/DPC++FPGA/Tutorials/Tools/experimental/platform-designer/build_design.sh
@@ -1,19 +1,26 @@
 #!/bin/bash -x
+cd add-oneapi &&
+mkdir build &&
+cd build &&
+cmake .. &&
+make fpga_ip_export &&
+cd ../../ &&
 
-cp -r add-oneapi/build/add.fpga_ip_export.prj_1/ add-quartus/
-cp -r add-oneapi/build/add.fpga_ip_export.prj_5/ add-quartus/
+cp -r add-oneapi/build/add.fpga_ip_export.prj_1/ add-quartus/ &&
+cp -r add-oneapi/build/add.fpga_ip_export.prj_5/ add-quartus/ &&
 
-cd add-quartus
+cd add-quartus &&
 
-cd add.fpga_ip_export.prj_1
-python add_fpga_ip_export_1_di_hw_tcl_adjustment_script.py 
+cd add.fpga_ip_export.prj_1 &&
+python add_fpga_ip_export_1_di_hw_tcl_adjustment_script.py  &&
 
-cd ..
-cd add.fpga_ip_export.prj_5
-python add_fpga_ip_export_5_di_hw_tcl_adjustment_script.py
+cd .. &&
+cd add.fpga_ip_export.prj_5 &&
+python add_fpga_ip_export_5_di_hw_tcl_adjustment_script.py &&
 
-cd ..
+cd .. &&
 
-qsys-script --script=add_kernels.tcl --quartus-project=AddCSRDemo.qpf
-qsys-generate add_kernels.qsys --synthesis --quartus-project=AddCSRDemo.qpf --rev=add
-quartus_sh --flow compile AddCSRDemo.qpf -c add
+qsys-script --script=add_kernels.tcl --quartus-project=AddCSRDemo.qpf &&
+# qsys-generate add_kernels.qsys --synthesis --quartus-project=AddCSRDemo.qpf --rev=add &&
+quartus_sh --flow compile AddCSRDemo.qpf -c add &&
+echo "DONE build_designs.sh"
