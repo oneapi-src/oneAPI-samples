4ca9541fe1788ed431814b0173a7233286ce4be2
White, Paul <paul.white@intel.com>
2022-11-18 07:50:45 -0800

    set permission on build_design.sh

--- DirectProgramming/DPC++FPGA/Tutorials/Tools/experimental/platform-designer/build_design.sh
+++ DirectProgramming/DPC++FPGA/Tutorials/Tools/experimental/platform-designer/build_design.sh
@@ -7,7 +7,6 @@ make fpga_ip_export &&
 cd ../../ &&
 
 cp -r add-oneapi/build/add.fpga_ip_export.prj_1/ add-quartus/ &&
-cp -r add-oneapi/build/add.fpga_ip_export.prj_5/ add-quartus/ &&
 
 cd add-quartus &&
 
@@ -15,12 +14,7 @@ cd add.fpga_ip_export.prj_1 &&
 python add_fpga_ip_export_1_di_hw_tcl_adjustment_script.py  &&
 
 cd .. &&
-cd add.fpga_ip_export.prj_5 &&
-python add_fpga_ip_export_5_di_hw_tcl_adjustment_script.py &&
 
-cd .. &&
-
-qsys-script --script=add_kernels.tcl --quartus-project=AddCSRDemo.qpf &&
-# qsys-generate add_kernels.qsys --synthesis --quartus-project=AddCSRDemo.qpf --rev=add &&
-quartus_sh --flow compile AddCSRDemo.qpf -c add &&
+qsys-script --script=add_kernel_wrapper.tcl --quartus-project=add.qpf &&
+quartus_sh --flow compile add.qpf -c add &&
 echo "DONE build_designs.sh"
