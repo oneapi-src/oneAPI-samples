46d50a3bed0bb07a157266ef33e5ed8cd685b214
ManjulaChalla <100553269+ManjulaChalla@users.noreply.github.com>
2022-11-29 17:49:19 +0530

    Update MonteCarloMultiGPU.cpp

--- DirectProgramming/DPC++/MapReduce/guided_MonteCarloMultiGPU_SYCLMigration/02_sycl_dpct_migrated/src/MonteCarloMultiGPU.cpp
+++ DirectProgramming/DPC++/MapReduce/guided_MonteCarloMultiGPU_SYCLMigration/02_sycl_dpct_migrated/src/MonteCarloMultiGPU.cpp
@@ -76,11 +76,11 @@ int adjustProblemSize(int GPU_N, int default_nOptions) {
 
     dpct::dev_mgr::instance().get_device(i).get_device_info(deviceProp);
 
-    int cudaCores =
+    int Cores =
         device.get_info<cl::sycl::info::device::max_compute_units>();
 
-    if (cudaCores <= 32) {
-      nOptions = (nOptions < cudaCores / 2 ? nOptions : cudaCores / 2);
+    if (Cores <= 32) {
+      nOptions = (nOptions < Cores / 2 ? nOptions : Cores / 2);
     }
   }
 
