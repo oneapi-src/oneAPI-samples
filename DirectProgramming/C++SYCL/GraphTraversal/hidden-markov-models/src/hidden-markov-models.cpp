61c79daac24def58498c31a3a4b276342471b436
Jimmy Wei <jimmy.t.wei@intel.com>
2023-01-13 15:17:08 -0800

    Update hidden-markov-models.cpp

--- DirectProgramming/C++SYCL/GraphTraversal/hidden-markov-models/src/hidden-markov-models.cpp
+++ DirectProgramming/C++SYCL/GraphTraversal/hidden-markov-models/src/hidden-markov-models.cpp
@@ -4,27 +4,27 @@
 // SPDX-License-Identifier: MIT
 // =============================================================
 //
-// Hidden Markov Models: this code sample implements the Viterbi algorithm which is a dynamic
+// Hidden Markov Models: this code sample implements the Viterbi algorithm which is a dynamic 
 // programming algorithm for finding the most likely sequence of hidden states—
 // called the Viterbi path—that results in a sequence of observed events,
 // especially in the context of Markov information sources and HMM.
 //
 // The sample can use GPU offload to compute sequential steps of multiple graph traversals simultaneously.
 //
-// - Initially, the dataset for algorithm processing is generated : initial states probability
-// distribution Pi, transition matrix A, emission matrix B and the sequence or the observations
+// - Initially, the dataset for algorithm processing is generated : initial states probability 
+// distribution Pi, transition matrix A, emission matrix B and the sequence or the observations 
 // produced by hidden Markov process.
-// - First, the matrix of Viterbi values on the first states are initialized using distribution Pi
+// - First, the matrix of Viterbi values on the first states are initialized using distribution Pi 
 // and emission matrix B. The matrix of back pointers is initialized with default values -1.
 // - Then, for each time step the Viterbi matrix is set to the maximal possible value using A, B and Pi.
-// - Finally, the state with maximum Viterbi value on the last step is set as a final state of
-// the Viterbi path and the previous nodes of this path are detemined using the correspondent rows
+// - Finally, the state with maximum Viterbi value on the last step is set as a final state of 
+// the Viterbi path and the previous nodes of this path are detemined using the correspondent rows 
 // of back pointers matrix for each of the steps except the last one.
 //
 // Note: The implementation uses logarithms of the probabilities to process small numbers correctly
 // and to replace multiplication operations with addition operations.
 
-#include <sycl/sycl.hpp>
+#include <sycl/sycl.hpp> 
 #include <iostream>
 #include <limits>
 #include <math.h>
@@ -111,16 +111,16 @@ int main() {
             h.parallel_for(range<2>(N, T), [=](id<2> index) {
                 int i = index[0];
                 int j = index[1];
-                // At starting point only the first Viterbi values are defined and these Values are substituted
+                // At starting point only the first Viterbi values are defined and these Values are substituted 
                 // with logarithms  due to the following equation: log(x*y) = log(x) + log(y).
                 v_acc[index] = (j != 0) ? MIN_DOUBLE : pi_acc[i] + b_acc[i][seq_acc[0]];
-                // Default values of all the back pointers are (-1) to show that they are not determined yet.
+                // Default values of all the back pointers are (-1) to show that they are not determined yet. 
                 b_ptr_acc[index] = -1;
             });
-        });
+        }); 
 
-        // The sequential steps of the Viterbi algorithm that define the Viterbi matrix and the matrix
-        // of back pointers. The product of the Viterbi values and the probabilities is substituted with the sum of
+        // The sequential steps of the Viterbi algorithm that define the Viterbi matrix and the matrix 
+        // of back pointers. The product of the Viterbi values and the probabilities is substituted with the sum of 
         // the logarithms due to the following equation: log (x*y*z) = log(x) + log(y) + log(z).
         for (int j = 0; j < T - 1; ++j) {
             q.submit([&](handler& h) {
@@ -132,7 +132,7 @@ int main() {
 
                 h.parallel_for(range<2>(N, N), [=](id<2> index) {
                     int i = index[0], k = index[1];
-                    // This conditional block finds the maximum possible Viterbi value on
+                    // This conditional block finds the maximum possible Viterbi value on 
                     // the current step j for the state i.
                     if (ViterbiCondition(v_acc[k][j], b_acc[i][seq_acc[j + 1]], a_acc[k][i], v_acc[i][j + 1])) {
                         v_acc[i][j + 1] = v_acc[k][j] + a_acc[k][i] + b_acc[i][seq_acc[j + 1]];
@@ -148,7 +148,7 @@ int main() {
         auto b_ptr_acc = back_pointer.get_access<access::mode::read>();
         auto vit_path_acc = vit_path.get_access<access::mode::read_write>();
         double v_max = MIN_DOUBLE;
-        // Constructing the Viterbi path. The last state of this path is the one with
+        // Constructing the Viterbi path. The last state of this path is the one with 
         // the biggest Viterbi value (the most likely state).
         for (int i = 0; i < N; ++i) {
             if (v_acc[i][T - 1] > v_max) {
@@ -179,8 +179,8 @@ int main() {
     return 0;
 }
 
-// The method checks if all three components of the sum are not equivalent to logarithm of zero
-// (that is incorrect value and is substituted with minimal possible value of double) and that
+// The method checks if all three components of the sum are not equivalent to logarithm of zero 
+// (that is incorrect value and is substituted with minimal possible value of double) and that 
 // the Viterbi value on the new step exceeds the current one.
 bool ViterbiCondition(double x, double y, double z, double compare) {
     return (x > MIN_DOUBLE) && (y > MIN_DOUBLE) && (z > MIN_DOUBLE) && (x + y + z > compare);
