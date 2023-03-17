ac42ff246ecac5108508fd421cb5972daf9f9903
michael vincerra <michael.vincerra@intel.com>
2023-03-17 11:33:34 -0700

    Test setup, oneAPI-samples
    Signed-off-by: michael vincerra <michael.vincerra@intel.com>

---
+++ src/docs/conf.py
@@ -0,0 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import json
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'oneAPI Samples'
+copyright = '2023, Intel'
+author = 'Intel'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+extensions = []
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+
+# html_theme = "" # pydata_sphinx_theme
+
+html_static_path = ['_static']
+
+master_doc = "index"
+
+root_doc = 'contents'
+
+html_additional_pages = {'index': 'index.html'}
+
+html_js_files =  ['_static/script.js']
+
+json_data = json.load(open('_static/sample_db_prd.json'))
+
+html_context = {
+    'data': json_data
+}
+
+def setup(app):
+    app.add_js_file('script.js', loading_method="async")
+    
