78eaa89f22e44a49bcbf0f6058b813fa94c20fcc
Michael Kinsner <michael.kinsner@intel.com>
2023-03-24 17:50:03 -0300

    Add second edition example of host_task

---
+++ samples/Ch02_where_code_runs/fig_2_23_host_task.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2020 Intel Corporation
+
+// SPDX-License-Identifier: MIT
+
+#include <sycl/sycl.hpp>
+#include <array>
+#include <iostream>
+using namespace sycl;
+constexpr int N = 4;
+
+int main() {
+
+  queue Q{gpu_selector_v};
+  int *A = malloc_shared<int>(N, Q);
+
+  std::cout << "Selected device: " <<
+    Q.get_device().get_info<info::device::name>() << "\n";
+
+  // Initialize values in the buffer
+  auto eA = Q.submit([&](handler& h) {
+      h.parallel_for(N , [=](auto& idx) {
+	  A[idx] = idx;
+	  });
+      });
+
+  // Use a host task to output values on the host as part of
+  // task graph.  depends_on is used to define a dependence on
+  // previous device code having completed.
+  // Here the host task is defined as a lambda.
+  Q.submit([&](handler& h) {
+      h.depends_on( eA );
+      h.host_task( [&](){
+	  for (int i = 0; i < N; i++)
+	  std::cout << "host_task @ " << i << " = " << A[i] << "\n";
+	  });
+      });
+
+  // Wait for work to be completed in the queue before accessing
+  // the shared data in the host program.
+  Q.wait();
+
+  for (int i = 0; i < N; i++)
+    std::cout << "main @ " << i << " = " << A[i] << "\n";
+
+  free(A, Q);
+
+  return 0;
+}
