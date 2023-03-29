1b315448375c2616ebaa91392a4b94ca9324df56
michael vincerra <michael.vincerra@intel.com>
2023-03-28 18:21:21 -0700

    test oneapi ci job Tue Mar 28 18:21:17 PDT 2023
    Signed-off-by: michael vincerra <michael.vincerra@intel.com>

--- src/docs/_static/script.js
+++ src/docs/_static/script.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     fetchData().then((data) => { /* */
         renderUi(data); /* */
         console.log(data)
+        getDateTime() 
     });
 });
 
@@ -57,3 +58,36 @@ function renderCards(data) {
         }
     })
 }
+
+function getDateTime() {
+    var now     = new Date(); 
+    var year    = now.getFullYear();
+    var month   = now.getMonth()+1; 
+    var day     = now.getDate();
+    var hour    = now.getHours();
+    var minute  = now.getMinutes();
+    var second  = now.getSeconds(); 
+    if(month.toString().length == 1) {
+         month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+         day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+         hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+         minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0'+second;
+    }   
+    var dateTime = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;   
+     return dateTime;
+}
+
+// example usage: realtime clock
+setInterval(function(){
+    currentTime = getDateTime();
+    document.getElementById("digital-clock").innerHTML = currentTime;
+}, 1000);
