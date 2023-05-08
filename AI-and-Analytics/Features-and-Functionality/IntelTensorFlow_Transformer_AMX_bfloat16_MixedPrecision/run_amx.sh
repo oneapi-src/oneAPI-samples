bab1931e4931537a7c1a53b54d0b29cba5edb3d6
Urszula Guminska <urszula.zofia.guminska@intel.com>
2023-05-08 13:17:55 +0200

    Changes to reflect tensorflow version 2.11

--- AI-and-Analytics/Features-and-Functionality/IntelTensorFlow_Transformer_AMX_bfloat16_MixedPrecision/run_amx.sh
+++ AI-and-Analytics/Features-and-Functionality/IntelTensorFlow_Transformer_AMX_bfloat16_MixedPrecision/run_amx.sh
@@ -9,6 +9,6 @@ export DNNL_VERBOSE=2
 # enable JIT Dump
 export DNNL_JIT_DUMP=1
 
-DNNL_MAX_CPU_ISA=AVX512_CORE_AMX python ./text_classification_with_transformer.py cpu >> ./logs/log_cpu_bf16_avx512_amx.csv 2>&1
+DNNL_MAX_CPU_ISA=AMX_BF16 python ./text_classification_with_transformer.py cpu >> ./logs/log_cpu_bf16_amx.csv 2>&1
 
 echo "########## Done with the run"
