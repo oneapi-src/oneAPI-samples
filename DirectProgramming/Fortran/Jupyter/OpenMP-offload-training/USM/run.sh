765ee959d38d0ae6169d4093151a4d5f38cffd65
Benson Muite <bkmgit@users.noreply.github.com>
2023-05-12 22:11:27 +0300

    Fix path
    The file main.f90 is in the current folder, not in lab

--- DirectProgramming/Fortran/Jupyter/OpenMP-offload-training/USM/run.sh
+++ DirectProgramming/Fortran/Jupyter/OpenMP-offload-training/USM/run.sh
@@ -2,7 +2,7 @@
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) is running OMP_Offload Module4 1/2 -- USM main.cpp/main.f90
 echo "########## Compiling"
-ifx -fiopenmp -fopenmp-targets=spir64 lab/main.f90 -o bin/a.out || exit $?
+ifx -fiopenmp -fopenmp-targets=spir64 main.f90 -o bin/a.out || exit $?
 echo "########## Executing"
 cd bin
 ./a.out
