3d4f7482eb67f9fa51a48af7a0e7b171d901c431
Benson Muite <bkmgit@users.noreply.github.com>
2023-05-26 20:22:54 +0300

    Add timing information
    Transform to be similar to C++ USM demonstration code

--- DirectProgramming/Fortran/Jupyter/OpenMP-offload-training/USM/main.f90
+++ DirectProgramming/Fortran/Jupyter/OpenMP-offload-training/USM/main.f90
@@ -5,35 +5,61 @@
 ! =============================================================
 program main
     use omp_lib
+    implicit none
     integer, parameter :: N=16
+    integer :: correct_count=0
     integer :: i
-    integer, allocatable :: x(:)
-    logical :: is_cpu = .true.
+    integer, allocatable :: x(:), y(:)
+    double precision :: te, tb
 
     !$omp allocate allocator(omp_target_shared_mem_alloc)
-    allocate(x(N)) 
+    allocate(x(N),y(N)) 
+    
+    print *,'Number of OpenMP Devices ',omp_get_num_devices()
+    
+    tb = omp_get_wtime()
     
     do i=1,N
-        x(i) = i
-    end do  
+        x(i) = 1
+    end do
+    
+    do i=1,N
+        y(i) = 1
+    end do
 
     !$omp target map(tofrom: is_cpu) has_device_addr(x)
+    !$omp target map(tofrom: is_cpu) has_device_addr(y)
     !$omp teams distribute parallel do
     do i=1,N
-        if ((i==1) .and. (.not.(omp_is_initial_device()))) is_cpu=.false.
-        x(i) = x(i) * 2
+        x(i) = x(i) + y(i)
     end do
     !$omp end target
-        
-    if (is_cpu) then
-        print *, "Running on CPU"
-    else
-        print *, "Running on GPU"
-    end if
-        
+    
     do i=1,N
-        print *, x(i)
+        y(i) = 2
+    end do
+        
+    !$omp target map(tofrom: is_cpu) has_device_addr(y)
+    !$omp teams distribute parallel do
+    do i=1,N
+        x(i) = x(i) + y(i)
+    end do
+    !$omp end target
+
+    te = omp_get_wtime()
+    print *,'Time of kernel ',te-tb,' seconds'
+    
+    do i=1,N
+        if (x(i)==4) then
+              correct_count = correct_count + 1
+        end if
     end do
     
-    deallocate(x)
+    if (correct_count==N) then
+         print *, 'Test: PASSED'
+    else
+          print *, 'Test: Failed'
+    endif
+    
+    deallocate(x,y)
 end program main
