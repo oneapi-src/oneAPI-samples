711e8d3072356ca9683a502804b3a96e413fdaeb
ManjulaChalla <manjula.challa98@gmail.com>
2023-06-27 02:36:36 -0700

    added oddevenmergesort sample
    Signed-off-by: ManjulaChalla <manjula.challa98@gmail.com>

---
+++ DirectProgramming/C++SYCL/GraphTraversal/guided_odd_even_merge_sort_SYCLMigration/02_sycl_migrated/include/dpct/dpct.hpp
@@ -0,0 +1,21 @@
+//==---- dpct.hpp ---------------------------------*- C++ -*----------------==//
+//
+// Copyright (C) Intel Corporation
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef __DPCT_HPP__
+#define __DPCT_HPP__
+
+#include <sycl/sycl.hpp>
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+
+#include "device.hpp"
+#include "memory.hpp"
+#include "util.hpp"
+
+#endif // __DPCT_HPP__
