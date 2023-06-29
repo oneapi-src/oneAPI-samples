57a292dac98b87ad9e1ad3d5bd939581ba34e090
ShwethaSelma <shwethasshetty27@gmail.com>
2023-06-28 23:39:28 -0700

    updated HSOpticalflow sample with new format
    Signed-off-by: ShwethaSelma <shwethasshetty27@gmail.com>

---
+++ DirectProgramming/C++SYCL/StructuredGrids/guided_HSOpticalFlow_SYCLMigration/02_sycl_migrated/include/dpct/util.hpp
@@ -0,0 +1,47 @@
+//==---- util.hpp ---------------------------------*- C++ -*----------------==//
+//
+// Copyright (C) Intel Corporation
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef __DPCT_UTIL_HPP__
+#define __DPCT_UTIL_HPP__
+
+#include <sycl/sycl.hpp>
+#include <complex>
+#include <type_traits>
+#include <cassert>
+#include <cstdint>
+
+
+namespace dpct {
+
+namespace detail {
+
+template <typename tag, typename T> class generic_error_type {
+public:
+  generic_error_type() = default;
+  generic_error_type(T value) : value{value} {}
+  operator T() const { return value; }
+
+private:
+  T value;
+};
+
+} // namespace detail
+
+using err0 = detail::generic_error_type<struct err0_tag, int>;
+using err1 = detail::generic_error_type<struct err1_tag, int>;
+
+template <int... Ints> struct integer_sequence {};
+template <int Size, int... Ints>
+struct make_index_sequence
+    : public make_index_sequence<Size - 1, Size - 1, Ints...> {};
+template <int... Ints>
+struct make_index_sequence<0, Ints...> : public integer_sequence<Ints...> {};
+
+} // namespace dpct
+
+#endif // __DPCT_UTIL_HPP__
