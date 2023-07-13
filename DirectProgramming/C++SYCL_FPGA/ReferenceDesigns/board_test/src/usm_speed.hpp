f669214615d8664c890b826276cba98f87eb11fb
William Wang <william1.wang@intel.com>
2023-07-13 08:51:07 -0700

    Addressing comments

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/board_test/src/usm_speed.hpp
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/board_test/src/usm_speed.hpp
@@ -1,19 +1,26 @@
+// Forward declare the kernel names
 class USMMemCopy;
 class USMMemRead;
 class USMMemWrite;
 
-using long8 = sycl::vec<long, 8>;
+// NOTE: sycl::ulong8 was picked for this test because it is 64 bytes in size
+// and that is the width of the interconnect to global memory.
 
 enum USMTest { MEMCOPY, READ, WRITE };
+
 constexpr size_t kIterations = 1;
 constexpr size_t kNumBytes = 1024 * 1024 * 1024;
-constexpr size_t kNumItems = kNumBytes / sizeof(long8);
+constexpr size_t kNumItems = kNumBytes / sizeof(sycl::ulong8);
+
+// Arbitrary value used for testing/initialization
+constexpr long kTestValue = 5;
 
 // MEMCOPY test: launches a kernel to copy data from one USM pointer to another.
-sycl::event memcopy_kernel(sycl::queue &q, long8 *in, long8 *out) {
+sycl::event memcopy_kernel(sycl::queue &q, sycl::ulong8 *in,
+                           sycl::ulong8 *out) {
   return q.single_task<USMMemCopy>([=]() [[intel::kernel_args_restrict]] {
-    sycl::host_ptr<long8> in_h(in);
-    sycl::host_ptr<long8> out_h(out);
+    sycl::host_ptr<sycl::ulong8> in_h(in);
+    sycl::host_ptr<sycl::ulong8> out_h(out);
     for (size_t i = 0; i < kNumItems; i++) {
       out_h[i] = in_h[i];
     }
@@ -22,11 +29,11 @@ sycl::event memcopy_kernel(sycl::queue &q, long8 *in, long8 *out) {
 
 // READ test: launches a kernel to read data from a USM pointer, sum it up, and
 // store to an output pointer.
-sycl::event read_kernel(sycl::queue &q, long8 *in, long8 *out) {
+sycl::event read_kernel(sycl::queue &q, sycl::ulong8 *in, sycl::ulong8 *out) {
   return q.single_task<USMMemRead>([=]() {
-    sycl::host_ptr<long8> in_h(in);
-    sycl::host_ptr<long8> out_h(out);
-    long8 sum{0};
+    sycl::host_ptr<sycl::ulong8> in_h(in);
+    sycl::host_ptr<sycl::ulong8> out_h(out);
+    sycl::ulong8 sum{0};
     for (size_t i = 0; i < kNumItems; i++) {
       sum += in_h[i];
     }
@@ -36,10 +43,10 @@ sycl::event read_kernel(sycl::queue &q, long8 *in, long8 *out) {
 }
 
 // WRITE test: launches a kernel to write data to a USM pointer.
-sycl::event write_kernel(sycl::queue &q, long8 *in, long8 *out) {
+sycl::event write_kernel(sycl::queue &q, sycl::ulong8 *in, sycl::ulong8 *out) {
   return q.single_task<USMMemWrite>([=]() {
-    sycl::host_ptr<long8> out_h(out);
-    long8 answer{5};
+    sycl::host_ptr<sycl::ulong8> out_h(out);
+    sycl::ulong8 answer{kTestValue};
     for (size_t i = 0; i < kNumItems; i++) {
       out_h[i] = answer;
     }
@@ -47,49 +54,42 @@ sycl::event write_kernel(sycl::queue &q, long8 *in, long8 *out) {
 }
 
 // Function to check the answer of the memcopy, read or write test.
-bool verify(long8 *in, long8 *out, USMTest test) {
-
-  // The write kernel writes a known value to every index of "out".
-  long8 write_answer{5};
-
+bool verify(sycl::ulong8 *in, sycl::ulong8 *out, USMTest test) {
   // The read kernel calculates a sum of all the values at "in" and stores it
-  // at out[0]. First calculate a reference to compare to. The rest of the
-  // indices should retain their intial value, 0. So, also define a zero vector
-  // to compare to.
-  long8 zero_vec{0};
-  long8 read_answer{0};
+  // at out[0]; first calculate a reference to compare to.
+  sycl::ulong8 read_answer{0};
   for (int i = 0; i < kNumItems; i++) {
     read_answer += in[i];
   }
 
   for (int i = 0; i < kNumItems; i++) {
-    // for each element of the output pointer, the value we expect it to take.
-    long8 reference;
+    // The actual value of each element of the output pointer and the value we
+    // expect it to take, respectively.
+    sycl::ulong8 actual = out[i];
+    sycl::ulong8 expected;
 
     switch (test) {
     case MEMCOPY:
-      // output pointer should match the input pointer.
-      reference = in[i];
+      // Output pointer should match the input pointer.
+      expected = in[i];
       break;
     case READ:
-      // first index should contain the expected answer (sum of all values at
+      // First index should contain the expected answer (sum of all values at
       // the input pointer); at all other it should contain zero.
-      reference = (i == 0) ? read_answer : zero_vec;
+      expected = (i == 0) ? read_answer : (sycl::ulong8)(0);
       break;
     case WRITE:
-      // each index should contain the known answer that was written.
-      reference = write_answer;
+      // Each index should contain the known answer that was written.
+      expected = kTestValue;
       break;
     }
 
-    // "compare" will be a vector containing an element-wise "==" of the
-    // vectors we are checking equality for.
-    long8 compare = (out[i] == reference);
-    for (int j = 0; j < compare.size(); j++) {
-      if (!compare[j]) {
+    // Verify the vectors are equal
+    for (int j = 0; j < 8; j++) {
+      if (actual[j] != expected[j]) {
         std::cerr << "ERROR: Values do not match, "
                   << "out[" << i << "][" << j << "] = " << out[i][j]
-                  << "; expected " << reference[j] << std::endl;
+                  << "; expected " << expected[j] << std::endl;
         return false;
       }
     }
@@ -105,27 +105,28 @@ int run_test(sycl::queue &q, USMTest test) {
   double kNumBytes_gb;
 
   // USM host allocation
-  long8 *in = sycl::malloc_host<long8>(kNumItems, q);
-  long8 *out = sycl::malloc_host<long8>(kNumItems, q);
+  sycl::ulong8 *in = sycl::malloc_host<sycl::ulong8>(kNumItems, q);
+  sycl::ulong8 *out = sycl::malloc_host<sycl::ulong8>(kNumItems, q);
   if (in == nullptr || out == nullptr) {
     std::cerr << "Error: Out of memory, can't allocate " << kNumBytes
               << " bytes" << std::endl;
     return 1;
   }
 
-  // Initialize the input with random values and output with zero
+  // Initialize the input with random values and output with zero.
   for (int i = 0; i < kNumItems; i++) {
     in[i] = rand();
-    out[i] = {0};
+    out[i] = 0;
   }
 
   // Test selection
-  std::function<sycl::event(sycl::queue &, long8 *, long8 *)> kernel;
+  std::function<sycl::event(sycl::queue &, sycl::ulong8 *, sycl::ulong8 *)>
+      kernel;
   switch (test) {
   case MEMCOPY:
     std::cout << "Case: Full Duplex" << std::endl;
     kernel = memcopy_kernel;
-    // full duplex transfers twice the amount of data
+    // Full duplex transfers twice the amount of data
     kNumBytes_gb = kNumBytes * 2 / kGB;
     break;
   case READ:
@@ -145,7 +146,8 @@ int run_test(sycl::queue &q, USMTest test) {
 
   std::cout << "Iterations: " << kIterations << std::endl;
   std::cout << "Data size: " << kNumBytes / kMB << " MB" << std::endl;
-  std::cout << "Data type size: " << sizeof(long8) << " bytes" << std::endl;
+  std::cout << "Data type size: " << sizeof(sycl::ulong8) << " bytes"
+            << std::endl;
 
   // The first iteration is slow due to one time tasks like buffer creation,
   // program creation and device programming, bandwidth measured in subsequent
