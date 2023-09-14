40d6e95e193cdb91e6032fc0e45205f88fc56329
AvijitBag07 <144990856+AvijitBag07@users.noreply.github.com>
2023-09-14 18:09:10 +0530

    Revisited Concurrent kernel (#1889)
    Signed-off-by: Shwetha-Selma <shwethax.selma@intel.com>
    Co-authored-by: Shwetha-Selma <shwethax.selma@intel.com>

--- DirectProgramming/C++SYCL/guided_concurrentKernels_SYCLMigration/02_sycl_migrated/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp
+++ DirectProgramming/C++SYCL/guided_concurrentKernels_SYCLMigration/02_sycl_migrated/Samples/0_Introduction/concurrentKernels/concurrentKernels.dp.cpp
@@ -33,55 +33,58 @@
 
 // Devices of compute capability 2.0 or higher can overlap the kernels
 //
-#include <helper_cuda.h>
-#include <helper_functions.h>
+#include <sycl/sycl.hpp>
+#include <dpct/dpct.hpp>
 #include <stdio.h>
 
+#include <helper_cuda.h>
+#include <helper_functions.h>
+#include <time.h>
 #include <chrono>
-#include <dpct/dpct.hpp>
-#include <sycl/sycl.hpp>
 
 // This is a kernel that does no real work but runs at least for a specified
-// number of clocks
-void clock_block(clock_t *d_o, clock_t clock_count) {
-  for (int i = 0; i < 500000; i++) {
-    d_o[0] = d_o[0] + i;
+// number of count
+void count_block(long *d_o, sycl::nd_item<3> &item_ct1) {
+  for (int i = item_ct1.get_local_id(2); i < 23000;
+       i+=1) {
+    auto dummy = sycl::sin((float)i) + sycl::cos((float)i);
+    d_o[0] = d_o[0] + i;    
   }
 }
 
 // Single warp reduction kernel
-void sum(clock_t *d_clocks, int N, const sycl::nd_item<3> &item_ct1,
-         clock_t *s_clocks) {
+void sum(long *d_count, int N, const sycl::nd_item<3> &item_ct1,
+         long *s_count) {
   // Handle to thread block group
-  auto cta = item_ct1.get_group();
+ // auto cta = item_ct1.get_group();
 
-  clock_t my_sum = 0;
+  long my_sum = 0;
 
   for (int i = item_ct1.get_local_id(2); i < N;
        i += item_ct1.get_local_range(2)) {
-    my_sum += d_clocks[i];
+    my_sum += d_count[i];
   }
 
-  s_clocks[item_ct1.get_local_id(2)] = my_sum;
+  s_count[item_ct1.get_local_id(2)] = my_sum;
+
   item_ct1.barrier();
 
   for (int i = 16; i > 0; i /= 2) {
     if (item_ct1.get_local_id(2) < i) {
-      s_clocks[item_ct1.get_local_id(2)] +=
-          s_clocks[item_ct1.get_local_id(2) + i];
+      s_count[item_ct1.get_local_id(2)] +=
+          s_count[item_ct1.get_local_id(2) + i];
     }
 
     item_ct1.barrier();
   }
 
-  d_clocks[0] = s_clocks[0];
+  d_count[0] = s_count[0];
 }
 
 int main(int argc, char **argv) {
-  sycl::queue q_ct1 = sycl::queue(sycl::default_selector_v);
   int nkernels = 8;             // number of concurrent kernels
   int nstreams = nkernels + 1;  // use one more stream than concurrent kernel
-  int nbytes = nkernels * sizeof(clock_t);  // number of data bytes
+  int nbytes = nkernels * sizeof(long);  // number of data bytes
   float kernel_time = 10;                   // time the kernel should run in ms
   float elapsed_time;                       // timing variables
   int cuda_device = 0;
@@ -96,48 +99,54 @@ int main(int argc, char **argv) {
 
   // use command-line specified CUDA device, otherwise use device with highest
   // Gflops/s
-  std::cout << "Device: "
-            << q_ct1.get_device().get_info<sycl::info::device::name>()
-            << std::endl;
+  //cuda_device = findCudaDevice(argc, (const char **)argv);
 
-  std::cout
-      << "> Detected Compute SM "
-      << q_ct1.get_device().get_info<sycl::info::device::version>()
-      << " hardware with "
-      << q_ct1.get_device().get_info<sycl::info::device::max_compute_units>()
-      << " multi-processors" << std::endl;
+  dpct::device_info deviceProp;
+  checkCudaErrors(cuda_device = dpct::dev_mgr::instance().current_device_id());
+
+  checkCudaErrors((dpct::dev_mgr::instance()
+                       .get_device(cuda_device)
+                       .get_device_info(deviceProp),
+                   0));
 
   if ((true == 0)) {
     printf("> GPU does not support concurrent kernel execution\n");
     printf("  CUDA kernel runs will be serialized\n");
   }
 
-  // allocate host memory
-  clock_t *a = 0;  // pointer to the array data in host memory
+  printf("> Detected Compute SM %d.%d hardware with %d multi-processors\n",
+         deviceProp.get_major_version(), deviceProp.get_minor_version(),
+         deviceProp.get_max_compute_units());
 
-  a = (clock_t *)sycl::malloc_host(nbytes, q_ct1);
+  // allocate host memory
+  long *a = 0;  // pointer to the array data in host memory
+  checkCudaErrors(
+      (a = (long *)sycl::malloc_host(nbytes, dpct::get_default_queue()), 0));
 
   // allocate device memory
-  clock_t *d_a = 0;  // pointers to data and init value in the device memory
+  long *d_a = 0;  // pointers to data and init value in the device memory
 
-  d_a = (clock_t *)sycl::malloc_device(nbytes, q_ct1);
+  checkCudaErrors(
+      (d_a = (long *)sycl::malloc_device(nbytes, dpct::get_default_queue()),
+       0));
 
   // allocate and initialize an array of stream handles
-  sycl::queue **streams =
-      (sycl::queue **)malloc(nstreams * sizeof(sycl::queue *));
+  dpct::queue_ptr *streams =
+      (dpct::queue_ptr *)malloc(nstreams * sizeof(dpct::queue_ptr));
 
   for (int i = 0; i < nstreams; i++) {
-    streams[i] = (sycl::queue *)malloc(nstreams * sizeof(sycl::queue));
-    *streams[i] =
-        sycl::queue(sycl::default_selector_v, dpct::exception_handler);
+
+    checkCudaErrors(
+        ((streams[i]) = dpct::get_current_device().create_queue(), 0));
   }
 
   // create CUDA event handles
   dpct::event_ptr start_event, stop_event;
   std::chrono::time_point<std::chrono::steady_clock> start_event_ct1;
   std::chrono::time_point<std::chrono::steady_clock> stop_event_ct1;
-  start_event = new sycl::event();
-  stop_event = new sycl::event();
+
+  checkCudaErrors((start_event = new sycl::event(), 0));
+  checkCudaErrors((stop_event = new sycl::event(), 0));
 
   // the events are used for synchronization only and hence do not need to
   // record timings this also makes events not introduce global sync points when
@@ -147,25 +156,24 @@ int main(int argc, char **argv) {
   kernelEvent = (dpct::event_ptr *)malloc(nkernels * sizeof(dpct::event_ptr));
 
   for (int i = 0; i < nkernels; i++) {
-    kernelEvent[i] = new sycl::event();
+    checkCudaErrors((kernelEvent[i] = new sycl::event(), 0));
   }
 
   //////////////////////////////////////////////////////////////////////
   // time execution with nkernels streams
-  clock_t total_clocks = 0;
+  long total_count = 0;
 #if defined(__arm__) || defined(__aarch64__)
   // the kernel takes more time than the channel reset time on arm archs, so to
-  // prevent hangs reduce time_clocks.
-  clock_t time_clocks = (clock_t)(kernel_time * (deviceProp.clockRate / 100));
+  // prevent hangs reduce time_count.
+  long time_count = (long)(kernel_time * (deviceProp.clockRate / 100));
 #else
-  clock_t time_clocks = (clock_t)(
-      kernel_time *
-      q_ct1.get_device().get_info<sycl::info::device::max_clock_frequency>());
+  long time_count =
+      (long)(kernel_time * deviceProp.get_max_clock_frequency());
 #endif
 
   sycl::event stop_event_streams_nstreams_1;
   start_event_ct1 = std::chrono::steady_clock::now();
-  *start_event = q_ct1.ext_oneapi_submit_barrier();
+  *start_event = dpct::get_default_queue().ext_oneapi_submit_barrier();
 
   // queue nkernels in separate streams and record when they are done
   for (int i = 0; i < nkernels; ++i) {
@@ -175,62 +183,67 @@ int main(int argc, char **argv) {
       cgh.parallel_for(
           sycl::nd_range<3>(sycl::range<3>(1, 1, 1), sycl::range<3>(1, 1, 1)),
           [=](sycl::nd_item<3> item_ct1) {
-            clock_block(d_a_i_ct0, time_clocks);
+            count_block(d_a_i_ct0, item_ct1);
           });
     });
-    total_clocks += time_clocks;
+    total_count += time_count;
 
     kernelEvent_ct1_i = std::chrono::steady_clock::now();
-
-    *kernelEvent[i] = streams[i]->ext_oneapi_submit_barrier();
+    checkCudaErrors(
+        (*kernelEvent[i] = streams[i]->ext_oneapi_submit_barrier(), 0));
 
     // make the last stream wait for the kernel event to be recorded
-
-    streams[nstreams - 1]->ext_oneapi_submit_barrier({*kernelEvent[i]});
+    checkCudaErrors(
+        (streams[nstreams - 1]->ext_oneapi_submit_barrier({*kernelEvent[i]}),
+         0));
   }
 
   // queue a sum kernel and a copy back to host in the last stream.
   // the commands in this stream get dispatched as soon as all the kernel events
   // have been recorded
   streams[nstreams - 1]->submit([&](sycl::handler &cgh) {
-    sycl::local_accessor<clock_t, 1> s_clocks_acc_ct1(sycl::range<1>(32), cgh);
+    sycl::local_accessor<long, 1> s_count_acc_ct1(sycl::range<1>(32), cgh);
 
     cgh.parallel_for(
         sycl::nd_range<3>(sycl::range<3>(1, 1, 32), sycl::range<3>(1, 1, 32)),
         [=](sycl::nd_item<3> item_ct1) {
-          sum(d_a, nkernels, item_ct1, s_clocks_acc_ct1.get_pointer());
+          sum(d_a, nkernels, item_ct1, s_count_acc_ct1.get_pointer());
         });
   });
 
-  stop_event_streams_nstreams_1 =
-      streams[nstreams - 1]->memcpy(a, d_a, sizeof(clock_t));
+  checkCudaErrors((stop_event_streams_nstreams_1 =
+                       streams[nstreams - 1]->memcpy(a, d_a, sizeof(long)),
+                   0));
 
   // at this point the CPU has dispatched all work for the GPU and can continue
   // processing other tasks in parallel
 
   // in this sample we just wait until the GPU is done
 
-  q_ct1.wait_and_throw();
+  dpct::get_current_device().queues_wait_and_throw();
   stop_event_streams_nstreams_1.wait();
   stop_event_ct1 = std::chrono::steady_clock::now();
+  checkCudaErrors(
+      (*stop_event = dpct::get_default_queue().ext_oneapi_submit_barrier(), 0));
+  checkCudaErrors(0);
 
-  *stop_event = q_ct1.ext_oneapi_submit_barrier();
-
-  elapsed_time =
-      std::chrono::duration<float, std::milli>(stop_event_ct1 - start_event_ct1)
-          .count();
+  checkCudaErrors((elapsed_time = std::chrono::duration<float, std::milli>(
+                                      stop_event_ct1 - start_event_ct1)
+                                      .count(),
+                   0));
 
   printf("Expected time for serial execution of %d kernels = %.3fs\n", nkernels,
          nkernels * kernel_time / 1000.0f);
   printf("Expected time for concurrent execution of %d kernels = %.3fs\n",
          nkernels, kernel_time / 1000.0f);
-  printf("Measured time for sample = %.5fs\n", elapsed_time / 1000.0f);
+  printf("Measured time for sample = %.3fs\n", elapsed_time / 1000.0f);
 
-  bool bTestResult = (a[0] > total_clocks);
+  bool bTestResult = (a[0] > total_count);
 
   // release resources
   for (int i = 0; i < nkernels; i++) {
-    free(streams[i]);
+    dpct::get_current_device().destroy_queue(streams[i]);
+    dpct::destroy_event(kernelEvent[i]);
   }
 
   free(streams);
@@ -238,8 +251,8 @@ int main(int argc, char **argv) {
 
   dpct::destroy_event(start_event);
   dpct::destroy_event(stop_event);
-  sycl::free(a, q_ct1);
-  sycl::free(d_a, q_ct1);
+  sycl::free(a, dpct::get_default_queue());
+  sycl::free(d_a, dpct::get_default_queue());
 
   if (!bTestResult) {
     printf("Test failed!\n");
