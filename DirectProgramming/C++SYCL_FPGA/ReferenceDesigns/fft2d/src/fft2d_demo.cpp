e7e447fc3760b2e3436fbab6f9a9c3263dae0360
Yohann Uguen <yohann.uguen@intel.com>
2023-10-27 00:45:54 -0700

    Revert "apply sophie's fix"
    This reverts commit 85866e3e49a1fa7af75d33077605e2b5e1bb926e.

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/fft2d/src/fft2d_demo.cpp
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/fft2d/src/fft2d_demo.cpp
@@ -239,7 +239,7 @@ void TestFFT(bool mangle, bool inverse) {
       auto fetch_event = q.single_task<class FetchKernel>(
           Fetch<kLogN, kLogParallelism, FetchToFFT, float>{to_read, mangle});
 
-      auto fft_event = q.single_task<class FFTKernel>(
+      q.single_task<class FFTKernel>(
           FFT<kLogN, kLogParallelism, FetchToFFT, FFTToTranspose, float>{
               inverse});
 
@@ -247,7 +247,6 @@ void TestFFT(bool mangle, bool inverse) {
           Transpose<kLogN, kLogParallelism, FFTToTranspose, float>{to_write,
                                                                    mangle});
 
-      fft_event.wait();
       transpose_event.wait();
 
       if (i == 0) {
