24989890263851b32d2726e679eb2e9921240365
Niu, Shuo <shuo.niu@intel.com>
2023-11-17 10:16:47 -0800

    Initial commit

--- DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/db/src/query9/query9_kernel.cpp
+++ DirectProgramming/C++SYCL_FPGA/ReferenceDesigns/db/src/query9/query9_kernel.cpp
@@ -463,7 +463,6 @@ bool SubmitQuery9(queue& q, Database& dbinfo, std::string colour,
       bool done = false;
       size_t num_rows = 0;
 
-      [[intel::initiation_interval(1)]]
       do {
         // get data from upstream
         bool valid;
@@ -506,7 +505,7 @@ bool SubmitQuery9(queue& q, Database& dbinfo, std::string colour,
           //    A) Apply the [[intel::speculated_iterations(0)]] attribute
           //    B) Explicitly bound the loop iterations
           // For an explanation why, see the optimize_inner_loops tutorial.
-          [[intel::initiation_interval(1), intel::speculated_iterations(0)]]
+          [[intel::speculated_iterations(0)]]
           for (char i = 0; i < valid_count && 
                 i < kLineItemOrdersJoinWinSize; i++) {
             UnrolledLoop<0, kLineItemOrdersJoinWinSize>([&](auto j) {
