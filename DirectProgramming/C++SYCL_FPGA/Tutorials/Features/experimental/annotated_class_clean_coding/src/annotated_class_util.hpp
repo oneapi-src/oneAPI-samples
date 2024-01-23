7e8a8ee32a17d414fd643d056f7f4a8c21b11da2
Wang, Di5 <di5.wang@intel.com>
2024-01-23 12:40:40 -0800

    format code

--- DirectProgramming/C++SYCL_FPGA/Tutorials/Features/experimental/annotated_class_clean_coding/src/annotated_class_util.hpp
+++ DirectProgramming/C++SYCL_FPGA/Tutorials/Features/experimental/annotated_class_clean_coding/src/annotated_class_util.hpp
@@ -3,7 +3,11 @@
 
 #include <sycl/sycl.hpp>
 
-using namespace sycl::ext::oneapi::experimental;
+template <typename T, typename P>
+using annotated_arg = sycl::ext::oneapi::experimental::annotated_arg<T, P>;
+
+template <typename T, typename P>
+using annotated_ptr = sycl::ext::oneapi::experimental::annotated_ptr<T, P>;
 
 namespace fpga_tools {
 ///////////////////////////////////////////////////////////////////////////////
@@ -51,40 +55,48 @@ namespace fpga_tools {
 //  annotated_arg_t c = fpga_tools::alloc_annotated<annotated_arg_t>(count, q);
 //
 //
-// NOTE: The template parameter for the "alloc_annotated" function must be either
-// an "annotated_arg" with a pointer type, or an "annotated_ptr". Other types
-// cause a compiler error.
+// NOTE: The template parameter for the "alloc_annotated" function must be
+// either an "annotated_arg" with a pointer type, or an "annotated_ptr". Other
+// types cause a compiler error.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 #if __cplusplus >= 202002L
 
-template <auto... Props> using properties_t = decltype(properties{Props...});
+template <auto... Props>
+using properties_t =
+    decltype(sycl::ext::oneapi::experimental::properties{Props...});
 
 #endif
 
 // Type traits to check if a type is annotated_ptr or
 // annotated_arg
-template <typename T> struct is_annotated_class : std::false_type {};
+template <typename T>
+struct is_annotated_class : std::false_type {};
 
 template <typename T, typename... Props>
-struct is_annotated_class<annotated_ptr<T, detail::properties_t<Props...>>>
+struct is_annotated_class<annotated_ptr<
+    T, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>>
     : std::true_type {};
 
 template <typename T, typename... Props>
-struct is_annotated_class<annotated_arg<T, detail::properties_t<Props...>>>
+struct is_annotated_class<annotated_arg<
+    T, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>>
     : std::true_type {};
 
 // Type traits to get the underlying raw type of annotated_arg/annotated_ptr
-template <typename T> struct get_raw_type {};
+template <typename T>
+struct get_raw_type {};
 
 template <typename T, typename... Props>
-struct get_raw_type<annotated_ptr<T, detail::properties_t<Props...>>> {
+struct get_raw_type<annotated_ptr<
+    T, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>> {
   using type = T;
 };
 
 template <typename T, typename... Props>
-struct get_raw_type<annotated_arg<T, detail::properties_t<Props...>>> {
+struct get_raw_type<annotated_arg<
+    T, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>> {
   static constexpr bool is_annotated_arg_for_pointer = false;
   static_assert(is_annotated_arg_for_pointer,
                 "'alloc_annotated' cannot be specified with annotated_arg<T> "
@@ -92,44 +104,55 @@ struct get_raw_type<annotated_arg<T, detail::properties_t<Props...>>> {
 };
 
 template <typename T, typename... Props>
-struct get_raw_type<annotated_arg<T *, detail::properties_t<Props...>>> {
+struct get_raw_type<annotated_arg<
+    T *, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>> {
   using type = T;
 };
 
 // Type traits to get the type of the property list in
 // annotated_arg/annotated_ptr
-template <typename T> struct get_property_list {};
+template <typename T>
+struct get_property_list {};
 
 template <typename T, typename... Props>
-struct get_property_list<annotated_ptr<T, detail::properties_t<Props...>>> {
-  using type = detail::properties_t<Props...>;
+struct get_property_list<annotated_ptr<
+    T, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>> {
+  using type = sycl::ext::oneapi::experimental::detail::properties_t<Props...>;
 };
 
 template <typename T, typename... Props>
-struct get_property_list<annotated_arg<T, detail::properties_t<Props...>>> {
-  using type = detail::properties_t<Props...>;
+struct get_property_list<annotated_arg<
+    T, sycl::ext::oneapi::experimental::detail::properties_t<Props...>>> {
+  using type = sycl::ext::oneapi::experimental::detail::properties_t<Props...>;
 };
 
 // Type traits to remove alignment from a property list. This is needed for
 // because the annotated malloc API does not support compile-time alignment
 // property
-template <typename T> struct remove_align_from {};
+template <typename T>
+struct remove_align_from {};
 
-template <> struct remove_align_from<detail::empty_properties_t> {
-  using type = detail::empty_properties_t;
+template <>
+struct remove_align_from<
+    sycl::ext::oneapi::experimental::detail::empty_properties_t> {
+  using type = sycl::ext::oneapi::experimental::detail::empty_properties_t;
 };
 
 template <typename Prop, typename... Props>
-struct remove_align_from<detail::properties_t<Prop, Props...>> {
+struct remove_align_from<
+    sycl::ext::oneapi::experimental::detail::properties_t<Prop, Props...>> {
   using type = std::conditional_t<
-      detail::HasAlign<detail::properties_t<Prop>>::value,
-      detail::properties_t<Props...>,
-      detail::merged_properties_t<
-          detail::properties_t<Prop>,
-          typename remove_align_from<detail::properties_t<Props...>>::type>>;
+      sycl::ext::oneapi::experimental::detail::HasAlign<
+          sycl::ext::oneapi::experimental::detail::properties_t<Prop>>::value,
+      sycl::ext::oneapi::experimental::detail::properties_t<Props...>,
+      sycl::ext::oneapi::experimental::detail::merged_properties_t<
+          sycl::ext::oneapi::experimental::detail::properties_t<Prop>,
+          typename remove_align_from<sycl::ext::oneapi::experimental::detail::
+                                         properties_t<Props...>>::type>>;
 };
 
-template <typename T> struct split_annotated_type {
+template <typename T>
+struct split_annotated_type {
   static constexpr bool is_valid_annotated_type = is_annotated_class<T>::value;
   static_assert(is_valid_annotated_type,
                 "alloc_annotated function only takes 'annotated_ptr' or "
@@ -138,7 +161,8 @@ template <typename T> struct split_annotated_type {
   using raw_type = typename get_raw_type<T>::type;
   using all_properties = typename get_property_list<T>::type;
   static constexpr size_t alignment =
-      detail::GetAlignFromPropList<all_properties>::value;
+      sycl::ext::oneapi::experimental::detail::GetAlignFromPropList<
+          all_properties>::value;
   using properties = typename remove_align_from<all_properties>::type;
 };
 
@@ -147,10 +171,10 @@ template <typename T> struct split_annotated_type {
 template <typename T>
 T alloc_annotated(size_t count, const sycl::queue &syclQueue,
                   sycl::usm::alloc usmKind = sycl::usm::alloc::host) {
-  auto ann_ptr =
-      aligned_alloc_annotated<typename split_annotated_type<T>::raw_type,
-                              typename split_annotated_type<T>::properties>(
-          split_annotated_type<T>::alignment, count, syclQueue, usmKind);
+  auto ann_ptr = sycl::ext::oneapi::experimental::aligned_alloc_annotated<
+      typename split_annotated_type<T>::raw_type,
+      typename split_annotated_type<T>::properties>(
+      split_annotated_type<T>::alignment, count, syclQueue, usmKind);
 
   if (ann_ptr.get() == nullptr) {
     std::cerr << "Memory allocation returns null" << std::endl;
@@ -160,6 +184,6 @@ T alloc_annotated(size_t count, const sycl::queue &syclQueue,
   return T{ann_ptr.get()};
 }
 
-} // namespace fpga_tools
+}  // namespace fpga_tools
 
 #endif
--- DirectProgramming/C++SYCL_FPGA/Tutorials/Features/experimental/annotated_class_clean_coding/src/annotated_coding.cpp
+++ DirectProgramming/C++SYCL_FPGA/Tutorials/Features/experimental/annotated_class_clean_coding/src/annotated_coding.cpp
@@ -1,8 +1,8 @@
 #include <sycl/ext/intel/fpga_extensions.hpp>
 #include <sycl/sycl.hpp>
 
-#include "exception_handler.hpp"
 #include "annotated_class_util.hpp"
+#include "exception_handler.hpp"
 
 constexpr int kBL1 = 1;
 constexpr int kAlignment = 32;
@@ -12,18 +12,18 @@ constexpr int kWidth = 256;
 // and can be re-used in the annotated memory allocation in the host code.
 // This syntax works if you have added the `-std=c++20` flag to your compiler
 // command.
-using annotated_arg_t= sycl::ext::oneapi::experimental::annotated_arg<
+using annotated_arg_t = sycl::ext::oneapi::experimental::annotated_arg<
     int *, fpga_tools::properties_t<
-        sycl::ext::intel::experimental::buffer_location<kBL1>,
-        sycl::ext::intel::experimental::dwidth<kWidth>,
-        sycl::ext::oneapi::experimental::alignment<kAlignment>>>;
+               sycl::ext::intel::experimental::buffer_location<kBL1>,
+               sycl::ext::intel::experimental::dwidth<kWidth>,
+               sycl::ext::oneapi::experimental::alignment<kAlignment>>>;
 
 struct MyIP {
   annotated_arg_t a;
   int size;
 
   void operator()() const {
-    #pragma unroll 8
+#pragma unroll 8
     for (int i = 0; i < size; i++) {
       a[i] *= 2;
     }
@@ -34,63 +34,63 @@ bool checkResult(int *arr, int size) {
   bool passed = true;
   for (int i = 0; i < size; i++) {
     int golden = 2 * i;
-      if (arr[i] != golden) {
-        std::cout << "ERROR! At index: " << i << " , expected: " << golden
-                  << " , found: " << arr[i] << "\n";
-        passed = false;
-      }
+    if (arr[i] != golden) {
+      std::cout << "ERROR! At index: " << i << " , expected: " << golden
+                << " , found: " << arr[i] << "\n";
+      passed = false;
     }
-    return passed;
+  }
+  return passed;
 }
 
 bool runWithUsmMalloc(sycl::queue &q) {
-    // Create and initialize the host arrays
-    constexpr int kN = 8;
-    std::cout << "using aligned_alloc_shared to allocate a block of shared memory\n";
+  // Create and initialize the host arrays
+  constexpr int kN = 8;
+  std::cout
+      << "using aligned_alloc_shared to allocate a block of shared memory\n";
 
-    // The SYCL USM allocation API requires us to explicitly specify buffer_location
-    // and alignment when allocating the host array.
-    // Unless you explicitly define these as named constants in your device code
-    // (which is not very tidy), you may accidentally mis-match these properties
-    // between your host code and device code.
-    int *array_a = sycl::aligned_alloc_shared<int>(
-        kAlignment, kN, q,
-        sycl::ext::intel::experimental::property::usm::buffer_location(kBL1));
+  // The SYCL USM allocation API requires us to explicitly specify
+  // buffer_location and alignment when allocating the host array. Unless you
+  // explicitly define these as named constants in your device code (which is
+  // not very tidy), you may accidentally mis-match these properties between
+  // your host code and device code.
+  int *array_a = sycl::aligned_alloc_shared<int>(
+      kAlignment, kN, q,
+      sycl::ext::intel::experimental::property::usm::buffer_location(kBL1));
 
-    for (int i = 0; i < kN; i++) {
-      array_a[i] = i;
-    }
+  for (int i = 0; i < kN; i++) {
+    array_a[i] = i;
+  }
 
-    q.single_task(MyIP{array_a, kN}).wait();
-    bool passed = checkResult(array_a, kN);
-    if (passed) {
-
-    }
-    sycl::free(array_a, q);
-    return passed;
+  q.single_task(MyIP{array_a, kN}).wait();
+  bool passed = checkResult(array_a, kN);
+  if (passed) {
+  }
+  sycl::free(array_a, q);
+  return passed;
 }
 
 bool runWithAnnotatedAlloc(sycl::queue &q) {
-    // Create and initialize the host arrays
-    constexpr int kN = 8;
-    std::cout << "using fpga_tools::alloc_annotated to allocate a block of shared memory\n";
+  // Create and initialize the host arrays
+  constexpr int kN = 8;
+  std::cout << "using fpga_tools::alloc_annotated to allocate a block of "
+               "shared memory\n";
 
-    // The "alloc_annotated" function extracts the buffer location and alignment
-    // properties from the type alias annotated_arg_t, rather than forcing you to
-    // explicitly define them in your code. This ensures the properties of the
-    // returned pointer match with the annotations on the kernel arguments.
-    annotated_arg_t array_a = fpga_tools::alloc_annotated<annotated_arg_t>(kN, q);
+  // The "alloc_annotated" function extracts the buffer location and alignment
+  // properties from the type alias annotated_arg_t, rather than forcing you to
+  // explicitly define them in your code. This ensures the properties of the
+  // returned pointer match with the annotations on the kernel arguments.
+  annotated_arg_t array_a = fpga_tools::alloc_annotated<annotated_arg_t>(kN, q);
 
-    for (int i = 0; i < kN; i++) {
-      array_a[i] = i;
-    }
+  for (int i = 0; i < kN; i++) {
+    array_a[i] = i;
+  }
 
-    q.single_task(MyIP{array_a, kN}).wait();
-    bool passed = checkResult(array_a, kN);
-    sycl::free(array_a, q);
-    return passed;
+  q.single_task(MyIP{array_a, kN}).wait();
+  bool passed = checkResult(array_a, kN);
+  sycl::free(array_a, q);
+  return passed;
 }
-
 
 int main(void) {
 #if FPGA_SIMULATOR
