efdde819d5e43caca50c59dfd23f8320eb8fbee6
Harry Wang <harry.wang@intel.com>
2024-03-06 16:59:24 -0500

    FPGA: kernel_args_restrict tutorial, update with functor-styled kernel.

--- DirectProgramming/C++SYCL_FPGA/Tutorials/Features/kernel_args_restrict/src/kernel_args_restrict.cpp
+++ DirectProgramming/C++SYCL_FPGA/Tutorials/Features/kernel_args_restrict/src/kernel_args_restrict.cpp
@@ -16,8 +16,37 @@ using namespace sycl;
 
 // Forward declare the kernel names in the global scope.
 // This FPGA best practice reduces name mangling in the optimization reports.
-class KernelArgsRestrict;
-class KernelArgsNoRestrict;
+class KernelArgsRestrict_Lambda;
+class KernelArgsNoRestrict_Lambda;
+class KernelArgsRestrict_Functor;
+class KernelArgsNoRestrict_Functor;
+
+template <class InputAcc, class OutputAcc>
+struct ArgsRestrictFunctor {
+  InputAcc input;
+  OutputAcc output;
+  size_t len;
+
+  [[intel::kernel_args_restrict]] 
+  void operator()() const {
+    for (int idx = 0; idx < len; ++idx) {
+      output[idx] = input[idx];
+    }
+  };
+};
+
+template <class InputAcc, class OutputAcc>
+struct ArgsNoRestrictFunctor {
+  InputAcc input;
+  OutputAcc output;
+  size_t len;
+
+  void operator()() const {
+    for (int idx = 0; idx < len; ++idx) {
+      output[idx] = input[idx];
+    }
+  };
+};
 
 // Return the execution time of the event, in seconds
 double GetExecutionTime(const event &e) {
@@ -27,8 +56,9 @@ double GetExecutionTime(const event &e) {
   return kernel_time;
 }
 
-void RunKernels(size_t size, std::vector<int> &in, std::vector<int> &nr_out,
-                std::vector<int> &r_out) {
+void RunKernels(size_t size, std::vector<int> &in, std::vector<int> &nr_out_lambda,
+                std::vector<int> &r_out_lambda, std::vector<int> &nr_out_functor,
+                std::vector<int> &r_out_functor) {
 #if FPGA_SIMULATOR
   auto selector = sycl::ext::intel::fpga_simulator_selector_v;
 #elif FPGA_HARDWARE
@@ -49,15 +79,17 @@ void RunKernels(size_t size, std::vector<int> &in, std::vector<int> &nr_out,
               << std::endl;
 
     buffer in_buf(in);
-    buffer nr_out_buf(nr_out);
-    buffer r_out_buf(r_out);
+    buffer nr_out_buf_lambda(nr_out_lambda);
+    buffer r_out_buf_lambda(r_out_lambda);
+    buffer nr_out_buf_functor(nr_out_functor);
+    buffer r_out_buf_functor(r_out_functor);
 
     // submit the task that DOES NOT apply the kernel_args_restrict attribute
-    auto e_nr = q.submit([&](handler &h) {
+    auto e_nr_lambda = q.submit([&](handler &h) {
       accessor in_acc(in_buf, h, read_only);
-      accessor out_acc(nr_out_buf, h, write_only, no_init);
+      accessor out_acc(nr_out_buf_lambda, h, write_only, no_init);
 
-      h.single_task<KernelArgsNoRestrict>([=]() {
+      h.single_task<KernelArgsNoRestrict_Lambda>([=]() {
         for (size_t i = 0; i < size; i++) {
           out_acc[i] = in_acc[i];
         }
@@ -65,26 +97,50 @@ void RunKernels(size_t size, std::vector<int> &in, std::vector<int> &nr_out,
     });
 
     // submit the task that DOES apply the kernel_args_restrict attribute
-    auto e_r = q.submit([&](handler &h) {
+    auto e_r_lambda = q.submit([&](handler &h) {
       accessor in_acc(in_buf, h, read_only);
-      accessor out_acc(r_out_buf, h, write_only, no_init);
+      accessor out_acc(r_out_buf_lambda, h, write_only, no_init);
 
-      h.single_task<KernelArgsRestrict>([=]() [[intel::kernel_args_restrict]] {
+      h.single_task<KernelArgsRestrict_Lambda>([=]() [[intel::kernel_args_restrict]] {
         for (size_t i = 0; i < size; i++) {
           out_acc[i] = in_acc[i];
         }
       });
     });
 
+    auto e_nr_functor = q.submit([&](handler &h) {
+      accessor in_acc(in_buf, h, read_only);
+      accessor out_acc(nr_out_buf_functor, h, write_only, no_init);
+
+      h.single_task<KernelArgsNoRestrict_Functor>(
+        ArgsNoRestrictFunctor<decltype(in_acc), decltype(out_acc)>{in_acc, out_acc, size}
+      );
+    });
+
+    auto e_r_functor = q.submit([&](handler &h) {
+      accessor in_acc(in_buf, h, read_only);
+      accessor out_acc(r_out_buf_functor, h, write_only, no_init);
+
+      h.single_task<KernelArgsRestrict_Functor>(
+        ArgsRestrictFunctor<decltype(in_acc), decltype(out_acc)>{in_acc, out_acc, size}
+      );
+    });
+
     // measure the execution time of each kernel
     double size_mb = (size * sizeof(int)) / (1024 * 1024);
-    double nr_time = GetExecutionTime(e_nr);
-    double r_time = GetExecutionTime(e_r);
+    double nr_time_lambda = GetExecutionTime(e_nr_lambda);
+    double r_time_lambda = GetExecutionTime(e_r_lambda);
+    double nr_time_functor = GetExecutionTime(e_nr_functor);
+    double r_time_functor = GetExecutionTime(e_r_functor);
 
     std::cout << "Size of vector: " << size << " elements\n";
-    std::cout << "Kernel throughput without attribute: " << (size_mb / nr_time)
+    std::cout << "Lambda kernel throughput without attribute: " << (size_mb / nr_time_lambda)
               << " MB/s\n";
-    std::cout << "Kernel throughput with attribute: " << (size_mb / r_time)
+    std::cout << "Lambda kernel throughput with attribute: " << (size_mb / r_time_lambda)
+              << " MB/s\n";
+    std::cout << "Functor kernel throughput without attribute: " << (size_mb / nr_time_functor)
+              << " MB/s\n";
+    std::cout << "Functor kernel throughput with attribute: " << (size_mb / r_time_functor)
               << " MB/s\n";
 
   } catch (sycl::exception const &e) {
@@ -111,6 +167,25 @@ void RunKernels(size_t size, std::vector<int> &in, std::vector<int> &nr_out,
   // 'nr_out' and 'r_out' vectors).
 }
 
+// Checks if the content of two vectors are equal up to a specified size.
+// The function prints a message indicating the index at which the mismatch occurred, 
+// and the name of the kernel of which the result is checked, specified by `kernel_name`.
+bool isEqual(std::vector<int> &in, std::vector<int> &out, std::string &&kernel_name, size_t size) {
+  bool equal {true};
+  for (size_t i = 0; i < size; i++) {
+    if (in[i] != out[i]) {
+      std::cout << "FAILED: mismatch at entry " << i
+                << " of " << kernel_name
+                << " kernel output\n";
+      std::cout << " (" << in[i] << " != " << out[i]
+                << ", in[i] != out[i])\n";
+      equal = false;
+      break;
+    }
+  }
+  return equal;
+}
+
 int main(int argc, char* argv[]) {
   // size of vectors to copy, allow user to change it from the command line
 #if defined(FPGA_SIMULATOR)
@@ -123,43 +198,28 @@ int main(int argc, char* argv[]) {
 
   // input/output data
   std::vector<int> in(size);
-  std::vector<int> nr_out(size), r_out(size);
+  std::vector<int> nr_out_lambda(size), r_out_lambda(size);
+  std::vector<int> nr_out_functor(size), r_out_functor(size);
 
   // generate some input data
   std::iota(in.begin(), in.end(), 0);
 
   // clear the output data
-  std::fill(nr_out.begin(), nr_out.end(), -1);
-  std::fill(r_out.begin(), r_out.end(), -1);
+  std::fill(nr_out_lambda.begin(), nr_out_lambda.end(), -1);
+  std::fill(r_out_lambda.begin(), r_out_lambda.end(), -1);
+  std::fill(nr_out_functor.begin(), nr_out_functor.end(), -1);
+  std::fill(r_out_functor.begin(), r_out_functor.end(), -1);
 
   // Run the kernels
-  RunKernels(size, in, nr_out, r_out);
+  RunKernels(size, in, nr_out_lambda, r_out_lambda, nr_out_functor, r_out_functor);
 
   // validate the results
   bool passed = true;
 
-  for (size_t i = 0; i < size; i++) {
-    if (in[i] != nr_out[i]) {
-      std::cout << "FAILED: mismatch at entry " << i
-                << " of 'KernelArgsNoRestrict' kernel output\n";
-      std::cout << " (" << in[i] << " != " << nr_out[i]
-                << ", in[i] != out[i])\n";
-      passed = false;
-      break;
-    }
-  }
-  
-  for (size_t i = 0; i < size; i++) {
-    if (in[i] != r_out[i]) {
-      std::cout << "FAILED: mismatch at entry " << i
-                << " of 'KernelArgsRestrict' kernel output\n";
-      std::cout << " (" << in[i] << " != " << r_out[i]
-                << ", in[i] != out[i])\n";
-
-      passed = false;
-      break;
-    }
-  }
+  passed &= isEqual(in, nr_out_lambda, "KernelArgsNoRestrict_Lambda", size);
+  passed &= isEqual(in, r_out_lambda, "KernelArgsRestrict_Lambda", size);
+  passed &= isEqual(in, nr_out_functor, "KernelArgsNoRestrict_Functor", size);
+  passed &= isEqual(in, r_out_functor, "KernelArgsRestrict_Functor", size);
 
   if (passed) {
     std::cout << "PASSED\n";
