07f11bdddfa2ce9ba1bc40609bb0ae9830b4c80b
Jimmy Wei <jimmy.t.wei@intel.com>
2024-03-20 14:27:56 -0700

    Update run_CPU.sh

--- RenderingToolkit/IRTK_Learning_Path/4.1_OpenVKL_Intro_CPU_GPU/run_CPU.sh
+++ RenderingToolkit/IRTK_Learning_Path/4.1_OpenVKL_Intro_CPU_GPU/run_CPU.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) "is running vklMinimal_CPU_01 through 06"
-for i in {01..06}; do
-    case="vklMinimal_CPU_${i}"
+i=1
+while [ $i -le 6 ]; do
+    case=$(printf "%02d" $i)
     echo
-    echo "$case"
-    ./bin/vklMinimal_CPU_${i}
+    echo "vklMinimal_CPU_$case"
+    ./bin/vklMinimal_CPU_$case
+    i=$((i + 1))
 done
