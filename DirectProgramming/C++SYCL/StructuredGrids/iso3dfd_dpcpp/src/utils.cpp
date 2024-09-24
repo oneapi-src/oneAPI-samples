0823bae16685408979c374c0fe47aa87ccce2ab6
iochocki <igor.ochocki@intel.com>
2024-09-25 00:00:14 +0200

    iso3dfd hotfix

--- DirectProgramming/C++SYCL/StructuredGrids/iso3dfd_dpcpp/src/utils.cpp
+++ DirectProgramming/C++SYCL/StructuredGrids/iso3dfd_dpcpp/src/utils.cpp
@@ -159,7 +159,7 @@ bool WithinEpsilon(float* output, float* reference, const size_t dim_x,
   }
 
   error_file.close();
-  norm2 = sqrt(norm2);
+  norm2 = std::sqrt(norm2);
   if (error) std::cout << "error (Euclidean norm): " << norm2 << "\n";
   return error;
 }
