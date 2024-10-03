98f3173f257dedb6fddeb1b6c574067ec84c9712
Lakshmi Arunachalam <99835661+larunach@users.noreply.github.com>
2024-10-03 09:59:35 -0700

    Update run.sh

--- AI-and-Analytics/Getting-Started-Samples/IntelJAX_GettingStarted/run.sh
+++ AI-and-Analytics/Getting-Started-Samples/IntelJAX_GettingStarted/run.sh
@@ -3,4 +3,4 @@ conda activate jax
 git clone https://github.com/google/jax.git
 cd jax
 export PYTHONPATH=$PYTHONPATH:$(pwd)
- python examples/spmd_mnist_classifier_fromscratch.py
+python examples/spmd_mnist_classifier_fromscratch.py
